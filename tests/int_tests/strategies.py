ed27a4dd4889a9fdcfec34baa0ae834fca099f95
Azat Ibrakov <azatibrakov@gmail.com>
2021-08-31 18:16:41 +0300

    Add test for 'Int.__hash__' magic method

--- tests/int_tests/strategies.py
+++ tests/int_tests/strategies.py
@@ -1,5 +1,10 @@
+from hypothesis import strategies
+
 from tests.strategies import (decimal_int_strings_with_leading_zeros,
                               int_strings_with_bases)
+from utils import to_int_with_builtin_int
 
 decimal_int_strings_with_leading_zeros = decimal_int_strings_with_leading_zeros
 int_strings_with_bases = int_strings_with_bases
+ints_with_builtin_ints = strategies.builds(
+    to_int_with_builtin_int, decimal_int_strings_with_leading_zeros)
--- tests/utils.py
+++ tests/utils.py
@@ -1,5 +1,11 @@
+from typing import Tuple
+
 from rithm import Int
 
 
 def is_equivalent_to_builtin_int(value: Int, builtin: int) -> bool:
     return str(value) == str(builtin)
+
+
+def to_int_with_builtin_int(decimal_string: str) -> Tuple[Int, int]:
+    return Int(decimal_string), int(decimal_string)

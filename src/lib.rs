3d3154bc7edef8a90c53d2075a6ec0b4d9f3f4b4
Azat Ibrakov <azatibrakov@gmail.com>
2021-09-02 09:39:15 +0300

    Complete formatting

--- src/lib.rs
+++ src/lib.rs
@@ -44,7 +44,9 @@ impl Int {
 #[pyproto]
 impl PyNumberProtocol for Int {
     fn __abs__(&self) -> Int {
-        Int { 0: self.0.clone().abs() }
+        Int {
+            0: self.0.clone().abs(),
+        }
     }
 
     fn __add__(lhs: Int, rhs: Int) -> Int {

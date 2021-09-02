27d65a2999b9411d48163c0cebd1dfc4656f48c8
Azat Ibrakov <azatibrakov@gmail.com>
2021-09-02 08:11:14 +0300

    Add test for 'Int.__abs__' magic method

---
+++ tests/int_tests/test_abs.py
@@ -0,0 +1,15 @@
+from typing import Tuple
+
+from hypothesis import given
+
+from rithm import Int
+from tests.utils import is_equivalent_to_builtin_int
+from . import strategies
+
+
+@given(strategies.ints_with_builtin_ints)
+def test_connection_with_builtin(int_with_builtin_int: Tuple[Int, int]
+                                 ) -> None:
+    int_, builtin_int = int_with_builtin_int
+
+    assert is_equivalent_to_builtin_int(abs(int_), abs(builtin_int))

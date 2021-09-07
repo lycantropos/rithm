b01cddc19470a95f9ba3a6a6c8853cb14ebe86e9
Azat Ibrakov <azatibrakov@gmail.com>
2021-09-07 13:28:19 +0300

    Complete 'Int' class

--- rithm/__init__.py
+++ rithm/__init__.py
@@ -5,5 +5,12 @@ __version__ = '0.1.1'
 try:
     from ._rithm import Int
 except ImportError:
+    from math import gcd as _gcd
+
+
     class Int(int):
-        pass
+        def gcd(self, other: 'Int') -> 'Int':
+            return Int(_gcd(self, other))
+
+        def __repr__(self) -> str:
+            return f'rithm.Int(\'{self}\')'

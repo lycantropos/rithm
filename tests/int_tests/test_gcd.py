430009aceb3cc2a97d5854c5db443054fb0d6b50
Azat Ibrakov <azatibrakov@gmail.com>
2021-09-07 12:43:08 +0300

    Add test for 'Int.gcd' method

---
+++ tests/int_tests/test_gcd.py
@@ -0,0 +1,20 @@
+import math
+from typing import Tuple
+
+from hypothesis import given
+from rithm import Int
+from tests.utils import is_equivalent_to_builtin_int
+
+from . import strategies
+
+
+@given(strategies.ints_with_builtin_ints, strategies.ints_with_builtin_ints)
+def test_connection_with_builtin(first_int_with_builtin_int: Tuple[Int, int],
+                                 second_int_with_builtin_int: Tuple[Int, int]
+                                 ) -> None:
+    first_int, first_builtin_int = first_int_with_builtin_int
+    second_int, second_builtin_int = second_int_with_builtin_int
+
+    assert is_equivalent_to_builtin_int(first_int.gcd(second_int),
+                                        math.gcd(first_builtin_int,
+                                                 second_builtin_int))

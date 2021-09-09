be0366e4ccaa3e39fb23642c0cad2083728f286f
Azat Ibrakov <azatibrakov@gmail.com>
2021-09-09 16:05:43 +0300

    Add test for 'Int.__truediv__' magic method

---
+++ tests/int_tests/test_truediv.py
@@ -0,0 +1,26 @@
+import fractions
+from typing import Tuple
+
+import pytest
+from hypothesis import given
+from rithm import Int
+from tests.utils import is_equivalent_to_builtin_fraction
+
+from . import strategies
+
+
+@given(strategies.ints_with_builtin_ints, strategies.ints_with_builtin_ints)
+def test_connection_with_builtin(first_int_with_builtin_int: Tuple[Int, int],
+                                 second_int_with_builtin_int: Tuple[Int, int]
+                                 ) -> None:
+    first_int, first_builtin_int = first_int_with_builtin_int
+    second_int, second_builtin_int = second_int_with_builtin_int
+
+    try:
+        result = first_int / second_int
+    except ZeroDivisionError:
+        with pytest.raises(ZeroDivisionError):
+            first_builtin_int / second_builtin_int
+    else:
+        assert is_equivalent_to_builtin_fraction(
+            result, fractions.Fraction(first_builtin_int, second_builtin_int))
--- tests/utils.py
+++ tests/utils.py
@@ -1,10 +1,19 @@
+import fractions
 from typing import Tuple
 
-from rithm import Int
+from rithm import (Int,
+                   Fraction)
 
 
 def equivalence(left: bool, right: bool) -> bool:
     return left is right
+
+
+def is_equivalent_to_builtin_fraction(value: Fraction,
+                                      builtin: fractions.Fraction) -> bool:
+    return (is_equivalent_to_builtin_int(value.numerator, builtin.numerator)
+            and is_equivalent_to_builtin_int(value.denominator,
+                                             builtin.denominator))
 
 
 def is_equivalent_to_builtin_int(value: Int, builtin: int) -> bool:

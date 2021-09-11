e1500629860b168e2e08dab1fbfc6b84a618b6f8
Azat Ibrakov <azatibrakov@gmail.com>
2021-09-11 15:41:29 +0300

    Add test for 'Fraction.__sub__' magic method

---
+++ tests/fraction_tests/test_sub.py
@@ -0,0 +1,22 @@
+import fractions
+from typing import Tuple
+
+from hypothesis import given
+
+from rithm import Fraction
+from tests.utils import is_equivalent_to_builtin_fraction
+from . import strategies
+
+
+@given(strategies.fractions_with_builtin_fractions,
+       strategies.fractions_with_builtin_fractions)
+def test_connection_with_builtin(first_with_builtin
+                                 : Tuple[Fraction, fractions.Fraction],
+                                 second_with_builtin
+                                 : Tuple[Fraction, fractions.Fraction]
+                                 ) -> None:
+    first, first_builtin = first_with_builtin
+    second, second_builtin = second_with_builtin
+
+    assert is_equivalent_to_builtin_fraction(first - second,
+                                             first_builtin - second_builtin)

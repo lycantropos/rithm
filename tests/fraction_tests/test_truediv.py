dcaceefc39f2d2343e3b8f7a750cffb33cb8482e
Azat Ibrakov <azatibrakov@gmail.com>
2021-09-11 18:22:20 +0300

    Add test for 'Fraction.__truediv__' magic method

---
+++ tests/fraction_tests/test_truediv.py
@@ -0,0 +1,26 @@
+import fractions
+
+import pytest
+from hypothesis import given
+
+from tests.utils import (FractionWithBuiltin,
+                         is_equivalent_to_builtin_fraction)
+from . import strategies
+
+
+@given(strategies.fractions_with_builtin_fractions,
+       strategies.fractions_with_builtin_fractions)
+def test_connection_with_builtin(first_with_builtin: FractionWithBuiltin,
+                                 second_with_builtin: FractionWithBuiltin
+                                 ) -> None:
+    first, first_builtin = first_with_builtin
+    second, second_builtin = second_with_builtin
+
+    try:
+        result = first / second
+    except ZeroDivisionError:
+        with pytest.raises(ZeroDivisionError):
+            first_builtin / second_builtin
+    else:
+        assert is_equivalent_to_builtin_fraction(
+            result, fractions.Fraction(first_builtin, second_builtin))

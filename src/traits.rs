3d2e40b4627a86bb5ee067be753dfba579239fd6
Azat Ibrakov <azatibrakov@gmail.com>
2021-09-12 16:47:04 +0300

    Add 'CheckedDivRem' trait

--- src/traits.rs
+++ src/traits.rs
@@ -125,6 +125,27 @@ macro_rules! plain_checked_div_impl {
 
 plain_checked_div_impl!(i8 i16 i32 i64 i128 isize u8 u16 u32 u64 u128 usize);
 
+pub trait CheckedDivEuclid<Rhs = Self> {
+    type Output;
+
+    fn checked_div_euclid(self, divisor: Rhs) -> Self::Output;
+}
+
+macro_rules! plain_checked_div_euclid_impl {
+    ($($t:ty)*) => ($(
+        impl CheckedDivEuclid for $t {
+            type Output = Option<Self>;
+
+            #[inline(always)]
+            fn checked_div_euclid(self, divisor: Self) -> Self::Output {
+                <$t>::checked_div_euclid(self, divisor)
+            }
+        }
+    )*)
+}
+
+plain_checked_div_euclid_impl!(i8 i16 i32 i64 i128 isize u8 u16 u32 u64 u128 usize);
+
 pub trait CheckedRem<Rhs = Self> {
     type Output;
 

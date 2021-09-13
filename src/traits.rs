d1faa769a19d8c3ca57620d9def8131c295d9b3a
Azat Ibrakov <azatibrakov@gmail.com>
2021-09-13 19:24:13 +0300

    Add 'CheckedPow' trait

--- src/traits.rs
+++ src/traits.rs
@@ -168,6 +168,27 @@ macro_rules! plain_checked_div_euclid_impl {
 
 plain_checked_div_euclid_impl!(i8 i16 i32 i64 i128 isize u8 u16 u32 u64 u128 usize);
 
+pub trait CheckedPow<Exponent> {
+    type Output;
+
+    fn checked_pow(self, exponent: Exponent) -> Self::Output;
+}
+
+macro_rules! plain_checked_pow_impl {
+    ($($t:ty)*) => ($(
+        impl CheckedPow<u32> for $t {
+            type Output = Option<$t>;
+
+            #[inline(always)]
+            fn checked_pow(self, exponent: u32) -> Self::Output {
+                <$t>::checked_pow(self, exponent)
+            }
+        }
+    )*)
+}
+
+plain_checked_pow_impl!(i8 i16 i32 i64 i128 isize u8 u16 u32 u64 u128 usize);
+
 pub trait CheckedRem<Rhs = Self> {
     type Output;
 

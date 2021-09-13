0f168342f68bf0594b2273ff456ae63d6c679233
Azat Ibrakov <azatibrakov@gmail.com>
2021-09-13 16:58:50 +0300

    Complete test of 'Int.__pow__' magic method

--- tests/int_tests/strategies.py
+++ tests/int_tests/strategies.py
@@ -10,5 +10,7 @@ int_strings_with_bases = int_strings_with_bases
 ints = strategies.builds(Int, decimal_int_strings_with_leading_zeros)
 ints_with_builtin_ints = strategies.builds(
     to_int_with_builtin_int, decimal_int_strings_with_leading_zeros)
+negative_ints_with_builtin_ints = strategies.builds(
+    to_int_with_builtin_int, strategies.integers(-100, -1).map(str))
 non_negative_ints_with_builtin_ints = strategies.builds(
     to_int_with_builtin_int, strategies.integers(0, 100).map(str))
--- tests/int_tests/test_pow.py
+++ tests/int_tests/test_pow.py
@@ -1,6 +1,10 @@
+import fractions
+
+import pytest
 from hypothesis import given
 
 from tests.utils import (IntWithBuiltin,
+                         is_equivalent_to_builtin_fraction,
                          is_equivalent_to_builtin_int)
 from . import strategies
 
@@ -15,3 +19,21 @@ def test_non_negative_exponent_no_modulo_connection_with_builtin(
 
     assert is_equivalent_to_builtin_int(base ** exponent,
                                         base_builtin ** exponent_builtin)
+
+
+@given(strategies.ints_with_builtin_ints,
+       strategies.negative_ints_with_builtin_ints)
+def test_negative_exponent_no_modulo_connection_with_builtin(
+        base_with_builtin: IntWithBuiltin,
+        exponent_with_builtin: IntWithBuiltin) -> None:
+    base, base_builtin = base_with_builtin
+    exponent, exponent_builtin = exponent_with_builtin
+
+    try:
+        result = base ** exponent
+    except ZeroDivisionError:
+        with pytest.raises(ZeroDivisionError):
+            fractions.Fraction(base_builtin) ** exponent_builtin
+    else:
+        assert is_equivalent_to_builtin_fraction(
+            result, fractions.Fraction(base_builtin) ** exponent_builtin)

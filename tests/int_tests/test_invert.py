ea76ff1b6e1d3a7ad36474ea79ba0e4e269eca4a
Azat Ibrakov <azatibrakov@gmail.com>
2021-09-13 09:33:23 +0300

    Add test for 'Int.__invert__' magic method

---
+++ tests/int_tests/test_invert.py
@@ -0,0 +1,12 @@
+from hypothesis import given
+
+from tests.utils import (IntWithBuiltin,
+                         is_equivalent_to_builtin_int)
+from . import strategies
+
+
+@given(strategies.ints_with_builtin_ints)
+def test_connection_with_builtin(int_with_builtin_int: IntWithBuiltin) -> None:
+    int_, builtin_int = int_with_builtin_int
+
+    assert is_equivalent_to_builtin_int(~int_, ~builtin_int)

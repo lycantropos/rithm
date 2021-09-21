afe0a8ed804aa793d99e5d4e53b0f417316a38cb
Azat Ibrakov <azatibrakov@gmail.com>
2021-09-21 15:35:54 +0300

    Add 'CheckedDivRemEuclid' trait

--- src/traits.rs
+++ src/traits.rs
@@ -161,6 +161,31 @@ macro_rules! plain_checked_div_rem_impl {
 
 plain_checked_div_rem_impl!(i8 i16 i32 i64 i128 isize u8 u16 u32 u64 u128 usize);
 
+pub trait CheckedDivRemEuclid<Divisor = Self> {
+    type Output;
+
+    fn checked_div_rem_euclid(self, divisor: Divisor) -> Self::Output;
+}
+
+macro_rules! plain_checked_div_rem_euclid_impl {
+    ($($t:ty)*) => ($(
+        impl CheckedDivRemEuclid for $t {
+            type Output = Option<(Self, Self)>;
+
+            #[inline(always)]
+            fn checked_div_rem_euclid(self, divisor: Self) -> Self::Output {
+                if divisor.is_zero() {
+                    None
+                } else {
+                    Some((<$t>::div_euclid(self, divisor), <$t>::rem_euclid(self, divisor)))
+                }
+            }
+        }
+    )*)
+}
+
+plain_checked_div_rem_euclid_impl!(i8 i16 i32 i64 i128 isize u8 u16 u32 u64 u128 usize);
+
 pub trait CheckedPow<Exponent> {
     type Output;
 

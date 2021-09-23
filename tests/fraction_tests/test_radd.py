685bfbf090515a068451ea692d9e660f72f89d48
Azat Ibrakov <azatibrakov@gmail.com>
2021-09-23 04:54:07 +0300

    Add test for 'Fraction.__radd__' magic method

---
+++ tests/fraction_tests/test_radd.py
@@ -0,0 +1,10 @@
+from hypothesis import given
+
+from rithm import (Fraction,
+                   Int)
+from . import strategies
+
+
+@given(strategies.fractions, strategies.ints)
+def test_connection_with_add(fraction: Fraction, int_: Int) -> None:
+    assert int_ + fraction == fraction + int_

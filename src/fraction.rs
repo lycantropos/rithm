f30323e7d31d9425eb135a2560ab70a6cc3fc9a3
Azat Ibrakov <azatibrakov@gmail.com>
2021-09-23 20:31:06 +0300

    Complete 'Sub' trait implementation of 'Fraction' generic

--- src/fraction.rs
+++ src/fraction.rs
@@ -382,12 +382,74 @@ impl<
     type Output = Self;
 
     fn sub(self, subtrahend: Self) -> Self::Output {
-        let (numerator, denominator) = normalize_components_moduli::<Component>(
+        let (numerator, denominator) = normalize_components_moduli(
             self.numerator * subtrahend.denominator.clone()
                 - subtrahend.numerator * self.denominator.clone(),
             self.denominator * subtrahend.denominator,
         );
         Self {
+            numerator,
+            denominator,
+        }
+    }
+}
+
+impl<
+        Component: Clone
+            + DivisivePartialMagma
+            + Eq
+            + GcdMagma
+            + Oppositive
+            + MultiplicativeMonoid
+            + SubtractiveMagma,
+    > Sub<Component> for Fraction<Component>
+{
+    type Output = Self;
+
+    fn sub(self, other: Component) -> Self::Output {
+        let (numerator, denominator) = normalize_components_moduli::<Component>(
+            self.numerator - other * self.denominator.clone(),
+            self.denominator,
+        );
+        Self {
+            numerator,
+            denominator,
+        }
+    }
+}
+
+macro_rules! plain_sub_fraction_impl {
+    ($($t:ty)*) => ($(
+    impl Sub<Fraction<$t>> for $t {
+        type Output = Fraction<Self>;
+
+        fn sub(self, subtrahend: Fraction<Self>) -> Self::Output {
+            let (numerator, denominator) = normalize_components_moduli(
+                self * subtrahend.denominator.clone() - subtrahend.numerator,
+                subtrahend.denominator,
+            );
+            Self::Output {
+                numerator,
+                denominator,
+            }
+        }
+    }
+    )*)
+}
+
+plain_sub_fraction_impl!(i8 i16 i32 i64 i128 isize);
+
+impl<Digit: AdditiveDigit + Eq + GcdDigit, const SEPARATOR: char, const SHIFT: usize>
+    Sub<Fraction<BigInt<Digit, SEPARATOR, SHIFT>>> for BigInt<Digit, SEPARATOR, SHIFT>
+{
+    type Output = Fraction<Self>;
+
+    fn sub(self, subtrahend: Fraction<Self>) -> Self::Output {
+        let (numerator, denominator) = normalize_components_moduli(
+            self * subtrahend.denominator.clone() - subtrahend.numerator,
+            subtrahend.denominator,
+        );
+        Self::Output {
             numerator,
             denominator,
         }

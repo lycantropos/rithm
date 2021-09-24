afb9d6de4fe394e7b7214770683a9f721b05a3e1
Azat Ibrakov <azatibrakov@gmail.com>
2021-09-24 09:49:55 +0300

    Complete tests of comparison operators of 'Fraction' class

--- tests/fraction_tests/test_ge.py
+++ tests/fraction_tests/test_ge.py
@@ -1,13 +1,18 @@
+from typing import Union
+
 from hypothesis import given
 
 from tests.utils import (FractionWithBuiltin,
+                         IntWithBuiltin,
                          equivalence)
 from . import strategies
 
 
-@given(strategies.fractions_with_builtins, strategies.fractions_with_builtins)
+@given(strategies.fractions_with_builtins,
+       strategies.fractions_or_ints_with_builtins)
 def test_connection_with_builtin(first_with_builtin: FractionWithBuiltin,
-                                 second_with_builtin: FractionWithBuiltin
+                                 second_with_builtin
+                                 : Union[FractionWithBuiltin, IntWithBuiltin]
                                  ) -> None:
     first, first_builtin = first_with_builtin
     second, second_builtin = second_with_builtin

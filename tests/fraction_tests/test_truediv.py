80077ffda09db0b6ed6d17b8ec71109a31278a00
Azat Ibrakov <azatibrakov@gmail.com>
2021-09-24 11:03:13 +0300

    Complete test of 'Fraction.__truediv__' magic method

--- tests/fraction_tests/test_truediv.py
+++ tests/fraction_tests/test_truediv.py
@@ -1,16 +1,20 @@
 import fractions
+from typing import Union
 
 import pytest
 from hypothesis import given
 
 from tests.utils import (FractionWithBuiltin,
+                         IntWithBuiltin,
                          is_equivalent_to_builtin_fraction)
 from . import strategies
 
 
-@given(strategies.fractions_with_builtins, strategies.fractions_with_builtins)
+@given(strategies.fractions_with_builtins,
+       strategies.fractions_or_ints_with_builtins)
 def test_connection_with_builtin(first_with_builtin: FractionWithBuiltin,
-                                 second_with_builtin: FractionWithBuiltin
+                                 second_with_builtin
+                                 : Union[FractionWithBuiltin, IntWithBuiltin]
                                  ) -> None:
     first, first_builtin = first_with_builtin
     second, second_builtin = second_with_builtin

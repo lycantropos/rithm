e97054fd29c975077249d6a683ea1d8a5f83974c
Azat Ibrakov <azatibrakov@gmail.com>
2021-10-06 19:56:03 +0300

    Add test for 'Fraction.__float__' magic method

---
+++ tests/fraction_tests/test_float.py
@@ -0,0 +1,19 @@
+import pytest
+from hypothesis import given
+
+from tests.utils import FractionWithBuiltin
+from . import strategies
+
+
+@given(strategies.fractions_with_builtins)
+def test_connection_with_builtin(fraction_with_builtin: FractionWithBuiltin
+                                 ) -> None:
+    fraction, builtin_fraction = fraction_with_builtin
+
+    try:
+        result = float(fraction)
+    except OverflowError:
+        with pytest.raises(OverflowError):
+            float(builtin_fraction)
+    else:
+        assert result == float(builtin_fraction)

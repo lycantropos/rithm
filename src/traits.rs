6b6d933d6c7070b2ac08c94ba2615f5b06ccdc63
Azat Ibrakov <azatibrakov@gmail.com>
2021-10-11 10:07:35 +0300

    Add 'BitLength' trait

--- src/traits.rs
+++ src/traits.rs
@@ -96,6 +96,27 @@ macro_rules! plain_abs_impl {
 
 plain_abs_impl!(f32 f64 i8 i16 i32 i64 i128 isize);
 
+pub trait BitLength {
+    type Output;
+
+    fn bit_length(self) -> Self::Output;
+}
+
+macro_rules! plain_bit_length_impl {
+    ($($t:ty)*) => ($(
+        impl BitLength for $t {
+            type Output = usize;
+
+            #[inline(always)]
+            fn bit_length(self) -> Self::Output {
+                (<$t>::BITS as usize) - (self.leading_zeros() as usize)
+            }
+        }
+    )*)
+}
+
+plain_bit_length_impl!(i8 i16 i32 i64 i128 isize u8 u16 u32 u64 u128 usize);
+
 pub trait DivEuclid<Divisor = Self> {
     type Output;
 

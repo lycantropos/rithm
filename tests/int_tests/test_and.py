a503b7d2d17f85915dfeea1c1cbbf60c4a1d38ab
Azat Ibrakov <azatibrakov@gmail.com>
2021-10-16 14:18:43 +0300

    Add test for 'Int.__and__' magic method

---
+++ tests/int_tests/test_and.py
@@ -0,0 +1,15 @@
+from hypothesis import given
+
+from tests.utils import (IntWithBuiltin,
+                         is_equivalent_to_builtin_int)
+from . import strategies
+
+
+@given(strategies.ints_with_builtins, strategies.ints_with_builtins)
+def test_connection_with_builtin(first_with_builtin: IntWithBuiltin,
+                                 second_with_builtin: IntWithBuiltin) -> None:
+    first, first_builtin = first_with_builtin
+    second, second_builtin = second_with_builtin
+
+    assert is_equivalent_to_builtin_int(first & second,
+                                        first_builtin & second_builtin)

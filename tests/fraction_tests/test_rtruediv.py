9701e995ca4f261fe526ce41f8a0d4590666bd46
Azat Ibrakov <azatibrakov@gmail.com>
2021-10-18 23:47:00 +0300

    Complete test of 'Fraction.__rtruediv__' magic method

--- tests/fraction_tests/test_rtruediv.py
+++ tests/fraction_tests/test_rtruediv.py
@@ -1,15 +1,23 @@
+import pytest
 from hypothesis import given
 
-from rithm import (Fraction,
-                   Int)
+from tests.utils import (FractionWithBuiltin,
+                         IntWithBuiltin,
+                         is_equivalent_to_builtin_fraction)
 from . import strategies
 
 
-@given(strategies.fractions, strategies.ints)
-def test_properties(fraction: Fraction, int_: Int) -> None:
+@given(strategies.fractions_with_builtins, strategies.ints_with_builtins)
+def test_connection_with_builtin(fraction_with_builtin: FractionWithBuiltin,
+                                 int_with_builtin: IntWithBuiltin) -> None:
+    fraction, builtin_fraction = fraction_with_builtin
+    int_, builtin_int = int_with_builtin
+
     try:
         result = int_ / fraction
     except ZeroDivisionError:
-        assert not fraction
+        with pytest.raises(ZeroDivisionError):
+            builtin_int / builtin_fraction
     else:
-        assert result * fraction == int_
+        assert is_equivalent_to_builtin_fraction(
+            result, builtin_int / builtin_fraction)

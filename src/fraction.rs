ffd1e6e5c52af8bbc3f6764bddc8e7f82193cb6b
Azat Ibrakov <azatibrakov@gmail.com>
2021-10-19 00:14:06 +0300

    Complete 'CheckedRemEuclid' trait implementation of 'Fraction' generic

--- src/fraction.rs
+++ src/fraction.rs
@@ -427,6 +427,57 @@ impl<
     }
 }
 
+macro_rules! plain_checked_rem_euclid_fraction_impl {
+    ($($t:ty)*) => ($(
+        impl CheckedRemEuclid<Fraction<Self>> for $t
+        {
+            type Output = Option<Fraction<Self>>;
+
+            fn checked_rem_euclid(self, divisor: Fraction<Self>) -> Self::Output {
+                if divisor.is_zero() {
+                    None
+                } else {
+                    let (numerator, denominator) = normalize_components_moduli(
+                        (self * divisor.denominator)
+                            .checked_rem_euclid(divisor.numerator)?,
+                        divisor.denominator,
+                    );
+                    Some(Fraction::<Self> {
+                        numerator,
+                        denominator,
+                    })
+                }
+            }
+        }
+        )*)
+}
+
+plain_checked_rem_euclid_fraction_impl!(i8 i16 i32 i64 i128 isize u8 u16 u32 u64 u128 usize);
+
+impl<
+        Digit: Eq + EuclidDivisibleDigit + GcdDigit + MultiplicativeDigit,
+        const SEPARATOR: char,
+        const SHIFT: usize,
+    > CheckedRemEuclid<Fraction<Self>> for BigInt<Digit, SEPARATOR, SHIFT>
+{
+    type Output = Option<Fraction<Self>>;
+
+    fn checked_rem_euclid(self, divisor: Fraction<Self>) -> Self::Output {
+        if divisor.is_zero() {
+            None
+        } else {
+            let (numerator, denominator) = normalize_components_moduli(
+                (self * divisor.denominator.clone()).checked_rem_euclid(divisor.numerator)?,
+                divisor.denominator,
+            );
+            Some(Fraction::<Self> {
+                numerator,
+                denominator,
+            })
+        }
+    }
+}
+
 impl<Component: Clone + Display + Eq + Unitary> Display for Fraction<Component> {
     fn fmt(&self, formatter: &mut Formatter) -> std::fmt::Result {
         if self.denominator.is_one() {

d1bc9ba0cd21b4c1203fae0cc8cf0a06e0c1d572
Azat Ibrakov <azatibrakov@gmail.com>
2021-10-18 17:22:26 +0300

    Add test for 'Int.bit_length' method

---
+++ tests/int_tests/test_bit_length.py
@@ -0,0 +1,13 @@
+from hypothesis import given
+
+from tests.utils import (IntWithBuiltin,
+                         is_equivalent_to_builtin_int)
+from . import strategies
+
+
+@given(strategies.ints_with_builtins)
+def test_connection_with_builtin(int_with_builtin_int: IntWithBuiltin) -> None:
+    int_, builtin_int = int_with_builtin_int
+
+    assert is_equivalent_to_builtin_int(int_.bit_length(),
+                                        builtin_int.bit_length())

0cd78b049ab01f48e190fe3a64d524b8ae82d9e9
Azat Ibrakov <azatibrakov@gmail.com>
2021-10-18 23:35:13 +0300

    Add test for 'Fraction.__floordiv__' magic method

---
+++ tests/fraction_tests/test_floordiv.py
@@ -0,0 +1,28 @@
+from typing import Union
+
+import pytest
+from hypothesis import given
+
+from tests.utils import (FractionWithBuiltin,
+                         IntWithBuiltin,
+                         is_equivalent_to_builtin_int)
+from . import strategies
+
+
+@given(strategies.fractions_with_builtins,
+       strategies.fractions_or_ints_with_builtins)
+def test_connection_with_builtin(first_with_builtin: FractionWithBuiltin,
+                                 second_with_builtin
+                                 : Union[FractionWithBuiltin, IntWithBuiltin]
+                                 ) -> None:
+    first, first_builtin = first_with_builtin
+    second, second_builtin = second_with_builtin
+
+    try:
+        result = first // second
+    except ZeroDivisionError:
+        with pytest.raises(ZeroDivisionError):
+            first_builtin // second_builtin
+    else:
+        assert is_equivalent_to_builtin_int(
+            result, first_builtin // second_builtin)

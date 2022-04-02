bce097ad9f1e53cb529f5b9e281588847a7094be
Azat Ibrakov <azatibrakov@gmail.com>
2022-04-02 21:20:02 +0700

    Update dependencies

--- src/lib.rs
+++ src/lib.rs
@@ -15,7 +15,8 @@ use pyo3::class::PyObjectProtocol;
 use pyo3::exceptions::*;
 use pyo3::prelude::{pyclass, pymethods, pymodule, pyproto, PyModule, PyResult, Python};
 use pyo3::types::{PyBytes, PyFloat, PyLong, PyString};
-use pyo3::{ffi, AsPyPointer, Py, PyAny, PyErr, PyNativeType, PyRef, ToPyObject};
+use pyo3::{AsPyPointer, Py, PyAny, PyErr, PyNativeType, PyRef, ToPyObject};
+use pyo3_ffi as ffi;
 use pyo3::{IntoPy, PyNumberProtocol, PyObject};
 
 use crate::traits::{
@@ -23,6 +24,7 @@ use crate::traits::{
     CheckedPowRemEuclid, CheckedRemEuclid, CheckedShl, CheckedShr, FromStrRadix, Gcd, Oppositive,
     Pow, Unitary, Zeroable,
 };
+use pyo3::type_object::PyTypeObject;
 
 pub mod big_int;
 mod digits;
@@ -58,14 +60,15 @@ impl PyInt {
         match _value {
             None => Ok(PyInt(_BigInt::zero())),
             Some(value) => {
-                if _base.is_some() || value.is_instance::<PyString>()? {
+                let py = value.py();
+                if _base.is_some() || value.is_instance(PyString::type_object(py))? {
                     match _BigInt::from_str_radix(value.extract::<&str>()?, _base.unwrap_or(10)) {
                         Ok(value) => Ok(PyInt(value)),
                         Err(reason) => Err(PyValueError::new_err(reason.to_string())),
                     }
-                } else if value.is_instance::<PyInt>()? {
+                } else if value.is_instance(PyInt::type_object(py))? {
                     value.extract::<PyInt>()
-                } else if value.is_instance::<PyFloat>()? {
+                } else if value.is_instance(PyFloat::type_object(py))? {
                     Ok(PyInt(
                         _BigInt::try_from(value.extract::<&PyFloat>()?.value()).map_err(
                             |reason| match reason {
@@ -176,7 +179,8 @@ impl PyFraction {
             },
             None => Ok(PyFraction(match _numerator {
                 Some(value) => {
-                    if value.is_instance::<PyInt>()? {
+                    let py = value.py();
+                    if value.is_instance(PyInt::type_object(py))? {
                         _Fraction::new(value.extract::<PyInt>()?.0, _BigInt::one()).unwrap()
                     } else {
                         _Fraction::try_from(value.extract::<f64>()?).map_err(
@@ -429,9 +433,9 @@ impl PyNumberProtocol for PyFraction {
 
     fn __add__(lhs: PyFraction, rhs: &PyAny) -> PyResult<PyObject> {
         let py = rhs.py();
-        if rhs.is_instance::<PyFraction>()? {
+        if rhs.is_instance(PyFraction::type_object(py))? {
             Ok(PyFraction(lhs.0 + rhs.extract::<PyFraction>()?.0).into_py(py))
-        } else if rhs.is_instance::<PyInt>()? {
+        } else if rhs.is_instance(PyInt::type_object(py))? {
             Ok(PyFraction(lhs.0 + rhs.extract::<PyInt>()?.0).into_py(py))
         } else {
             Ok(py.NotImplemented())
@@ -447,14 +451,14 @@ impl PyNumberProtocol for PyFraction {
 
     fn __floordiv__(lhs: PyFraction, rhs: &PyAny) -> PyResult<PyObject> {
         let py = rhs.py();
-        if rhs.is_instance::<PyFraction>()? {
+        if rhs.is_instance(PyFraction::type_object(py))? {
             match lhs.0.checked_div_euclid(rhs.extract::<PyFraction>()?.0) {
                 Some(value) => Ok(PyInt(value).into_py(py)),
                 None => Err(PyZeroDivisionError::new_err(
                     UNDEFINED_DIVISION_ERROR_MESSAGE,
                 )),
             }
-        } else if rhs.is_instance::<PyInt>()? {
+        } else if rhs.is_instance(PyInt::type_object(py))? {
             match lhs.0.checked_div_euclid(rhs.extract::<PyInt>()?.0) {
                 Some(value) => Ok(PyInt(value).into_py(py)),
                 None => Err(PyZeroDivisionError::new_err(
@@ -468,14 +472,14 @@ impl PyNumberProtocol for PyFraction {
 
     fn __mod__(lhs: PyFraction, rhs: &PyAny) -> PyResult<PyObject> {
         let py = rhs.py();
-        if rhs.is_instance::<PyFraction>()? {
+        if rhs.is_instance(PyFraction::type_object(py))? {
             match lhs.0.checked_rem_euclid(rhs.extract::<PyFraction>()?.0) {
                 Some(value) => Ok(PyFraction(value).into_py(py)),
                 None => Err(PyZeroDivisionError::new_err(
                     UNDEFINED_DIVISION_ERROR_MESSAGE,
                 )),
             }
-        } else if rhs.is_instance::<PyInt>()? {
+        } else if rhs.is_instance(PyInt::type_object(py))? {
             match lhs.0.checked_rem_euclid(rhs.extract::<PyInt>()?.0) {
                 Some(value) => Ok(PyFraction(value).into_py(py)),
                 None => Err(PyZeroDivisionError::new_err(
@@ -489,9 +493,9 @@ impl PyNumberProtocol for PyFraction {
 
     fn __mul__(lhs: PyFraction, rhs: &PyAny) -> PyResult<PyObject> {
         let py = rhs.py();
-        if rhs.is_instance::<PyFraction>()? {
+        if rhs.is_instance(PyFraction::type_object(py))? {
             Ok(PyFraction(lhs.0 * rhs.extract::<PyFraction>()?.0).into_py(py))
-        } else if rhs.is_instance::<PyInt>()? {
+        } else if rhs.is_instance(PyInt::type_object(py))? {
             Ok(PyFraction(lhs.0 * rhs.extract::<PyInt>()?.0).into_py(py))
         } else {
             Ok(py.NotImplemented())
@@ -517,7 +521,7 @@ impl PyNumberProtocol for PyFraction {
 
     fn __radd__(&self, other: &PyAny) -> PyResult<PyObject> {
         let py = other.py();
-        if other.is_instance::<PyInt>()? {
+        if other.is_instance(PyInt::type_object(py))? {
             Ok(PyFraction(other.extract::<PyInt>()?.0 + self.0.clone()).into_py(py))
         } else {
             Ok(py.NotImplemented())
@@ -526,7 +530,7 @@ impl PyNumberProtocol for PyFraction {
 
     fn __rfloordiv__(&self, other: &PyAny) -> PyResult<PyObject> {
         let py = other.py();
-        if other.is_instance::<PyInt>()? {
+        if other.is_instance(PyInt::type_object(py))? {
             match other
                 .extract::<PyInt>()?
                 .0
@@ -544,7 +548,7 @@ impl PyNumberProtocol for PyFraction {
 
     fn __rmod__(&self, other: &PyAny) -> PyResult<PyObject> {
         let py = other.py();
-        if other.is_instance::<PyInt>()? {
+        if other.is_instance(PyInt::type_object(py))? {
             match other
                 .extract::<PyInt>()?
                 .0
@@ -562,7 +566,7 @@ impl PyNumberProtocol for PyFraction {
 
     fn __rmul__(&self, other: &PyAny) -> PyResult<PyObject> {
         let py = other.py();
-        if other.is_instance::<PyInt>()? {
+        if other.is_instance(PyInt::type_object(py))? {
             Ok(PyFraction(other.extract::<PyInt>()?.0 * self.0.clone()).into_py(py))
         } else {
             Ok(py.NotImplemented())
@@ -571,7 +575,7 @@ impl PyNumberProtocol for PyFraction {
 
     fn __rsub__(&self, other: &PyAny) -> PyResult<PyObject> {
         let py = other.py();
-        if other.is_instance::<PyInt>()? {
+        if other.is_instance(PyInt::type_object(py))? {
             Ok(PyFraction(other.extract::<PyInt>()?.0 - self.0.clone()).into_py(py))
         } else {
             Ok(py.NotImplemented())
@@ -580,7 +584,7 @@ impl PyNumberProtocol for PyFraction {
 
     fn __rtruediv__(&self, other: &PyAny) -> PyResult<PyObject> {
         let py = other.py();
-        if other.is_instance::<PyInt>()? {
+        if other.is_instance(PyInt::type_object(py))? {
             match other.extract::<PyInt>()?.0.checked_div(self.0.clone()) {
                 Some(value) => Ok(PyFraction(value).into_py(py)),
                 None => Err(PyZeroDivisionError::new_err(
@@ -594,9 +598,9 @@ impl PyNumberProtocol for PyFraction {
 
     fn __sub__(lhs: PyFraction, rhs: &PyAny) -> PyResult<PyObject> {
         let py = rhs.py();
-        if rhs.is_instance::<PyFraction>()? {
+        if rhs.is_instance(PyFraction::type_object(py))? {
             Ok(PyFraction(lhs.0 - rhs.extract::<PyFraction>()?.0).into_py(py))
-        } else if rhs.is_instance::<PyInt>()? {
+        } else if rhs.is_instance(PyInt::type_object(py))? {
             Ok(PyFraction(lhs.0 - rhs.extract::<PyInt>()?.0).into_py(py))
         } else {
             Ok(py.NotImplemented())
@@ -605,14 +609,14 @@ impl PyNumberProtocol for PyFraction {
 
     fn __truediv__(lhs: PyFraction, rhs: &PyAny) -> PyResult<PyObject> {
         let py = rhs.py();
-        if rhs.is_instance::<PyFraction>()? {
+        if rhs.is_instance(PyFraction::type_object(py))? {
             match lhs.0.checked_div(rhs.extract::<PyFraction>()?.0) {
                 Some(value) => Ok(PyFraction(value).into_py(py)),
                 None => Err(PyZeroDivisionError::new_err(
                     UNDEFINED_DIVISION_ERROR_MESSAGE,
                 )),
             }
-        } else if rhs.is_instance::<PyInt>()? {
+        } else if rhs.is_instance(PyInt::type_object(py))? {
             match lhs.0.checked_div(rhs.extract::<PyInt>()?.0) {
                 Some(value) => Ok(PyFraction(value).into_py(py)),
                 None => Err(PyZeroDivisionError::new_err(
@@ -641,9 +645,9 @@ impl PyObjectProtocol for PyFraction {
 
     fn __richcmp__(&self, other: &PyAny, op: CompareOp) -> PyResult<PyObject> {
         let py = other.py();
-        if other.is_instance::<PyFraction>()? {
+        if other.is_instance(PyFraction::type_object(py))? {
             Ok(compare(&self.0, &other.extract::<PyFraction>()?.0, op).into_py(py))
-        } else if other.is_instance::<PyInt>()? {
+        } else if other.is_instance(PyInt::type_object(py))? {
             Ok(compare(&self.0, &other.extract::<PyInt>()?.0, op).into_py(py))
         } else {
             Ok(py.NotImplemented())

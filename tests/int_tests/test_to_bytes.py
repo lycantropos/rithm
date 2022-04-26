73fea35dc40d662b570f8c9a93d3b9a35df30ca1
Azat Ibrakov <azatibrakov@gmail.com>
2022-04-26 16:55:24 +0700

    Complete test of 'Int.to_bytes' method

--- tests/int_tests/test_to_bytes.py
+++ tests/int_tests/test_to_bytes.py
@@ -26,8 +26,12 @@ def test_connection_with_builtin(int_with_builtin_int: IntWithBuiltin,
     int_, builtin_int = int_with_builtin_int
 
     assert (int_.to_bytes(endianness)
-            == builtin_int.to_bytes(-(-builtin_int.bit_length() // 8) or 1,
+            == builtin_int.to_bytes(to_bytes_count(builtin_int),
                                     'big'
                                     if endianness is Endianness.BIG
                                     else 'little',
                                     signed=True))
+
+
+def to_bytes_count(value: int) -> int:
+    return (8 + (value + (value < 0)).bit_length()) // 8

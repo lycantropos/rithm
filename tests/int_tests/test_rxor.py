50dde10279a99a8e716826979b0a0959d61da564
Azat Ibrakov <azatibrakov@gmail.com>
2022-04-29 03:30:10 +0700

    Add test for 'Int.__rxor__' magic method

---
+++ tests/int_tests/test_rxor.py
@@ -0,0 +1,13 @@
+from hypothesis import given
+
+from rithm import Int
+from tests.utils import IntWithBuiltin
+from . import strategies
+
+
+@given(strategies.ints_with_builtins, strategies.ints)
+def test_connection_with_add(first_with_builtin: IntWithBuiltin, second: Int
+                             ) -> None:
+    first, first_builtin = first_with_builtin
+
+    assert first_builtin ^ second == first ^ second

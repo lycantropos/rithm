9ff8d14b06ad55dc982fc499c7d946c55128ddfc
Azat Ibrakov <azatibrakov@gmail.com>
2022-04-29 03:29:10 +0700

    Add test for 'Int.numerator' property

---
+++ tests/int_tests/test_numerator.py
@@ -0,0 +1,9 @@
+from hypothesis import given
+
+from rithm import Int
+from . import strategies
+
+
+@given(strategies.ints)
+def test_idempotence(int_: Int) -> None:
+    assert int_.numerator == int_

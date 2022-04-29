4527144281b4b9f8e337e4fbfe727a1ac929140d
Azat Ibrakov <azatibrakov@gmail.com>
2022-04-29 12:46:22 +0700

    Complete test of 'Int.__new__' magic method

--- tests/int_tests/test_new.py
+++ tests/int_tests/test_new.py
@@ -8,6 +8,12 @@ from tests.utils import is_equivalent_to_builtin_int
 from . import strategies
 
 
+def test_no_argument_determinism() -> None:
+    result = Int()
+
+    assert result == Int()
+
+
 def test_no_argument_connection_with_builtin() -> None:
     result = Int()
 
@@ -15,10 +21,28 @@ def test_no_argument_connection_with_builtin() -> None:
 
 
 @given(strategies.decimal_int_strings_with_leading_zeros)
+def test_decimal_string_determinism(string: str) -> None:
+    result = Int(string)
+
+    assert result == Int(string)
+
+
+@given(strategies.decimal_int_strings_with_leading_zeros)
 def test_decimal_string_connection_with_builtin(string: str) -> None:
     result = Int(string)
 
     assert is_equivalent_to_builtin_int(result, int(string))
+
+
+@given(strategies.floats)
+def test_float_determinism(float_: float) -> None:
+    try:
+        result = Int(float_)
+    except (OverflowError, ValueError) as exception:
+        with pytest.raises(type(exception)):
+            Int(float_)
+    else:
+        assert result == Int(float_)
 
 
 @given(strategies.floats)
@@ -30,6 +54,16 @@ def test_float_connection_with_builtin(float_: float) -> None:
             int(float_)
     else:
         assert is_equivalent_to_builtin_int(result, int(float_))
+
+
+@given(strategies.int_strings_with_bases)
+def test_string_with_base_determinism(string_with_base: Tuple[str, int]
+                                      ) -> None:
+    string, base = string_with_base
+
+    result = Int(string, base)
+
+    assert result == Int(string, base)
 
 
 @given(strategies.int_strings_with_bases)

9ff17817ed7faa7c78afb9650ae1aa8645466cf0
Azat Ibrakov <azatibrakov@gmail.com>
2022-05-02 06:52:16 +0700

    Complete test of 'Fraction.__divmod__' magic method

--- tests/fraction_tests/test__divmod__.py
+++ tests/fraction_tests/test__divmod__.py
@@ -1,13 +1,35 @@
 import pytest
 from hypothesis import given
 
-from rithm import Fraction
-from tests.utils import (FractionWithBuiltin,
+from rithm import (Fraction,
+                   Int)
+from tests.utils import (FractionOrIntOrBuiltinInt,
+                         FractionWithBuiltin,
                          IntWithBuiltin,
                          RationalWithBuiltin,
                          is_equivalent_to_builtin_fraction,
                          is_equivalent_to_builtin_int)
 from . import strategies
+
+
+@given(strategies.fractions,
+       strategies.non_zero_fractions_or_ints_or_builtin_ints)
+def test_basic(dividend: Fraction, divisor: FractionOrIntOrBuiltinInt) -> None:
+    result = divmod(dividend, divisor)
+
+    assert isinstance(result, tuple)
+    assert len(result) == 2
+    assert isinstance(result[0], Int)
+    assert isinstance(result[1], Fraction)
+
+
+@given(strategies.fractions,
+       strategies.non_zero_fractions_or_ints_or_builtin_ints)
+def test_alternatives(dividend: Fraction, divisor: FractionOrIntOrBuiltinInt
+                      ) -> None:
+    result = divmod(dividend, divisor)
+
+    assert result == (dividend // divisor, dividend % divisor)
 
 
 @given(strategies.fractions, strategies.ints_with_builtins)
@@ -42,3 +64,10 @@ def test_connection_with_builtin(dividend_with_builtin: FractionWithBuiltin,
 
         assert is_equivalent_to_builtin_int(quotient, builtin_quotient)
         assert is_equivalent_to_builtin_fraction(remainder, builtin_remainder)
+
+
+@given(strategies.fractions, strategies.zero_fractions_or_ints_or_builtin_ints)
+def test_zero_divisor(dividend: Fraction, divisor: FractionOrIntOrBuiltinInt
+                      ) -> None:
+    with pytest.raises(ZeroDivisionError):
+        divmod(dividend, divisor)

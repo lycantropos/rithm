e7cca8120c0221abd5a09337e1590a6a612cd5a4
Azat Ibrakov <azatibrakov@gmail.com>
2022-05-02 05:31:54 +0700

    Implement 'CheckedDivRemEuclid' trait for 'Fraction' generic

--- src/fraction.rs
+++ src/fraction.rs
@@ -349,6 +349,111 @@ impl<
 
 impl<
         Component: Clone
+            + CheckedDivRemEuclid<Output = Option<(Component, Component)>>
+            + DivisivePartialMagma
+            + Eq
+            + GcdMagma
+            + MultiplicativeMonoid
+            + Zeroable,
+    > CheckedDivRemEuclid for Fraction<Component>
+{
+    type Output = Option<(Component, Self)>;
+
+    fn checked_div_rem_euclid(self, divisor: Self) -> Self::Output {
+        let (quotient, remainder_numerator) = (self.numerator * divisor.denominator.clone())
+            .checked_div_rem_euclid(divisor.numerator * self.denominator.clone())?;
+        let (remainder_numerator, remainder_denominator) = normalize_components_moduli(
+            remainder_numerator,
+            self.denominator * divisor.denominator,
+        );
+        Some((
+            quotient,
+            Self {
+                numerator: remainder_numerator,
+                denominator: remainder_denominator,
+            },
+        ))
+    }
+}
+
+impl<
+        Component: Clone
+            + CheckedDivRemEuclid<Output = Option<(Component, Component)>>
+            + DivisivePartialMagma
+            + Eq
+            + GcdMagma
+            + MultiplicativeMonoid
+            + Zeroable,
+    > CheckedDivRemEuclid<Component> for Fraction<Component>
+{
+    type Output = Option<(Component, Self)>;
+
+    fn checked_div_rem_euclid(self, divisor: Component) -> Self::Output {
+        let (quotient, remainder_numerator) =
+            (self.numerator).checked_div_rem_euclid(divisor * self.denominator.clone())?;
+        let (remainder_numerator, remainder_denominator) =
+            normalize_components_moduli(remainder_numerator, self.denominator);
+        Some((
+            quotient,
+            Self {
+                numerator: remainder_numerator,
+                denominator: remainder_denominator,
+            },
+        ))
+    }
+}
+
+macro_rules! plain_checked_div_rem_euclid_fraction_impl {
+    ($($t:ty)*) => ($(
+    impl CheckedDivRemEuclid<Fraction<Self>> for $t
+    {
+        type Output = Option<(Self, Fraction<Self>)>;
+
+        fn checked_div_rem_euclid(self, divisor: Fraction<Self>) -> Self::Output {
+            let (quotient, remainder_numerator) = (self * divisor.denominator.clone())
+                .checked_div_rem_euclid(divisor.numerator)?;
+            let (remainder_numerator, remainder_denominator) = normalize_components_moduli(
+                remainder_numerator, divisor.denominator,
+            );
+            Some((
+                quotient,
+                Fraction::<Self> {
+                    numerator: remainder_numerator,
+                    denominator: remainder_denominator,
+                },
+            ))
+        }
+    }
+    )*)
+}
+
+plain_checked_div_rem_euclid_fraction_impl!(i8 i16 i32 i64 i128 isize u8 u16 u32 u64 u128 usize);
+
+impl<
+        Digit: Eq + EuclidDivisibleDigit + GcdDigit + MultiplicativeDigit,
+        const SEPARATOR: char,
+        const SHIFT: usize,
+    > CheckedDivRemEuclid<Fraction<Self>> for BigInt<Digit, SEPARATOR, SHIFT>
+{
+    type Output = Option<(Self, Fraction<Self>)>;
+
+    fn checked_div_rem_euclid(self, divisor: Fraction<Self>) -> Self::Output {
+        let (quotient, remainder_numerator) =
+            (self * divisor.denominator.clone()).checked_div_rem_euclid(divisor.numerator)?;
+        let (remainder_numerator, remainder_denominator) =
+            normalize_components_moduli(remainder_numerator, divisor.denominator);
+        Some((
+            quotient,
+            Fraction::<Self> {
+                numerator: remainder_numerator,
+                denominator: remainder_denominator,
+            },
+        ))
+    }
+}
+
+impl<
+        Component: Clone
             + Eq
             + Oppositive
             + CheckedPow<Component, Output = Option<Component>>

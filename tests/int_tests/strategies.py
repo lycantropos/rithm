5d8141e862e01adaa6d34b6007225487a439f1a7
Azat Ibrakov <azatibrakov@gmail.com>
2022-05-02 06:55:38 +0700

    Complete test of 'Int.__divmod__' magic method

--- tests/int_tests/strategies.py
+++ tests/int_tests/strategies.py
@@ -11,9 +11,15 @@ floats = strategies.floats()
 decimal_int_strings_with_leading_zeros = decimal_int_strings_with_leading_zeros
 int_strings_with_bases = int_strings_with_bases
 integers = strategies.integers()
+non_zero_integers = integers.filter(bool)
+zero_integers = strategies.builds(int)
 ints = integers.map(Int)
 ints_or_builtins = ints | integers
 ints_with_builtins = strategies.builds(to_int_with_builtin, integers)
+non_zero_ints = strategies.builds(Int, non_zero_integers)
+non_zero_ints_or_builtins = non_zero_ints | non_zero_integers
+zero_ints = strategies.builds(Int)
+zero_ints_or_builtins = zero_ints | zero_integers
 max_one_byte_signed_builtin_int = 1 << 7
 negative_one_byte_integers = strategies.integers(
         -max_one_byte_signed_builtin_int, -1
--- tests/int_tests/test__divmod__.py
+++ tests/int_tests/test__divmod__.py
@@ -2,9 +2,27 @@ import pytest
 from hypothesis import given
 
 from rithm import Int
-from tests.utils import (IntWithBuiltin,
+from tests.utils import (IntOrBuiltin,
+                         IntWithBuiltin,
                          is_equivalent_to_builtin_int)
 from . import strategies
+
+
+@given(strategies.ints, strategies.non_zero_ints_or_builtins)
+def test_basic(dividend: Int, divisor: IntOrBuiltin) -> None:
+    result = divmod(dividend, divisor)
+
+    assert isinstance(result, tuple)
+    assert len(result) == 2
+    assert isinstance(result[0], Int)
+    assert isinstance(result[1], Int)
+
+
+@given(strategies.ints, strategies.non_zero_ints_or_builtins)
+def test_alternatives(dividend: Int, divisor: IntOrBuiltin) -> None:
+    result = divmod(dividend, divisor)
+
+    assert result == (dividend // divisor, dividend % divisor)
 
 
 @given(strategies.ints, strategies.ints_with_builtins)
@@ -38,3 +56,9 @@ def test_connection_with_builtin(dividend_with_builtin: IntWithBuiltin,
 
         assert is_equivalent_to_builtin_int(quotient, builtin_quotient)
         assert is_equivalent_to_builtin_int(remainder, builtin_remainder)
+
+
+@given(strategies.ints, strategies.zero_ints_or_builtins)
+def test_zero_divisor(dividend: Int, divisor: IntOrBuiltin) -> None:
+    with pytest.raises(ZeroDivisionError):
+        divmod(dividend, divisor)

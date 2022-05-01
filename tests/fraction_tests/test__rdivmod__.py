a35e53525f759d734c3415d2f8fca94318f316e1
Azat Ibrakov <azatibrakov@gmail.com>
2022-05-02 06:47:26 +0700

    Add test for 'Fraction.__rdivmod__' magic method

---
+++ tests/fraction_tests/test__rdivmod__.py
@@ -0,0 +1,20 @@
+import pytest
+from hypothesis import given
+
+from rithm import Fraction
+from tests.utils import IntWithBuiltin
+from . import strategies
+
+
+@given(strategies.ints_with_builtins, strategies.fractions)
+def test_connection_with_divmod(dividend_with_builtin: IntWithBuiltin,
+                                divisor: Fraction) -> None:
+    dividend, dividend_builtin = dividend_with_builtin
+
+    try:
+        result = divmod(dividend_builtin, divisor)
+    except ZeroDivisionError as exception:
+        with pytest.raises(type(exception)):
+            divmod(dividend, divisor)
+    else:
+        assert result == divmod(dividend, divisor)

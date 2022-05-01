e30f756a5040d9de90c8f05dde0e3b2afd7a1a61
Azat Ibrakov <azatibrakov@gmail.com>
2022-05-01 22:11:27 +0700

    Complete tests names of 'Fraction' class

--- tests/fraction_tests/test__rmul__.py
+++ tests/fraction_tests/test__rmul__.py
@@ -6,5 +6,5 @@ from . import strategies
 
 
 @given(strategies.ints_or_builtins, strategies.fractions)
-def test_connection_with_mul(first: IntOrBuiltin, second: Fraction) -> None:
+def test_connection_with__mul__(first: IntOrBuiltin, second: Fraction) -> None:
     assert first * second == second * first
--- tests/fraction_tests/test__rsub__.py
+++ tests/fraction_tests/test__rsub__.py
@@ -6,5 +6,6 @@ from . import strategies
 
 
 @given(strategies.ints_or_builtins, strategies.fractions)
-def test_connection_with_radd(first: IntOrBuiltin, second: Fraction) -> None:
+def test_connection_with__radd__(first: IntOrBuiltin, second: Fraction
+                                 ) -> None:
     assert first - second == first + (-second)

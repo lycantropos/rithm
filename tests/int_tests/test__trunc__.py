7e9526ec772345344c706f709aadccfbf81608ba
Azat Ibrakov <azatibrakov@gmail.com>
2022-05-02 11:24:06 +0700

    Complete test of 'Int.__trunc__' magic method

--- tests/int_tests/test__trunc__.py
+++ tests/int_tests/test__trunc__.py
@@ -13,6 +13,14 @@ def test_basic(int_: Int) -> None:
     assert isinstance(math.trunc(int_), Int)
 
 
+@given(strategies.ints)
+def test_value(int_: Int) -> None:
+    result = math.trunc(int_)
+
+    assert abs(result - int_) < 1
+    assert result % 1 == 0
+
+
 @given(strategies.ints_with_builtins)
 def test_connection_with_builtin(int_with_builtin: IntWithBuiltin) -> None:
     int_, builtin_int = int_with_builtin

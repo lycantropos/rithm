d4874c56fab3ccfaaf6b5bf37181a89aed37bde7
Azat Ibrakov <azatibrakov@gmail.com>
2022-05-06 12:56:13 +0700

    Complete 'Mul' trait implementation of 'BigInt' generic

--- src/big_int.rs
+++ src/big_int.rs
@@ -1266,6 +1266,45 @@ impl<Digit: MultiplicativeDigit, const SEPARATOR: char, const SHIFT: usize> Mul
     }
 }
 
+impl<Digit: MultiplicativeDigit, const SEPARATOR: char, const SHIFT: usize> Mul<&Self>
+    for BigInt<Digit, SEPARATOR, SHIFT>
+{
+    type Output = Self;
+
+    fn mul(self, other: &Self) -> Self::Output {
+        Self::Output {
+            sign: self.sign * other.sign,
+            digits: multiply_digits::<Digit, SHIFT>(&self.digits, &other.digits),
+        }
+    }
+}
+
+impl<Digit: MultiplicativeDigit, const SEPARATOR: char, const SHIFT: usize>
+    Mul<BigInt<Digit, SEPARATOR, SHIFT>> for &BigInt<Digit, SEPARATOR, SHIFT>
+{
+    type Output = BigInt<Digit, SEPARATOR, SHIFT>;
+
+    fn mul(self, other: BigInt<Digit, SEPARATOR, SHIFT>) -> Self::Output {
+        Self::Output {
+            sign: self.sign * other.sign,
+            digits: multiply_digits::<Digit, SHIFT>(&self.digits, &other.digits),
+        }
+    }
+}
+
+impl<Digit: MultiplicativeDigit, const SEPARATOR: char, const SHIFT: usize> Mul
+    for &BigInt<Digit, SEPARATOR, SHIFT>
+{
+    type Output = BigInt<Digit, SEPARATOR, SHIFT>;
+
+    fn mul(self, other: Self) -> Self::Output {
+        Self::Output {
+            sign: self.sign * other.sign,
+            digits: multiply_digits::<Digit, SHIFT>(&self.digits, &other.digits),
+        }
+    }
+}
+
 impl<Digit: MultiplicativeDigit, const SEPARATOR: char, const SHIFT: usize> MulAssign
     for BigInt<Digit, SEPARATOR, SHIFT>
 {

125997beb672a40ab655200e7488fc8ede236bec
Azat Ibrakov <azatibrakov@gmail.com>
2022-05-06 11:33:27 +0700

    Complete 'Add' trait implementation of 'BigInt' generic

--- src/big_int.rs
+++ src/big_int.rs
@@ -456,6 +456,42 @@ impl<Digit: AdditiveDigit, const SEPARATOR: char, const SHIFT: usize> Add
     }
 }
 
+impl<Digit: AdditiveDigit, const SEPARATOR: char, const SHIFT: usize> Add<&Self>
+    for BigInt<Digit, SEPARATOR, SHIFT>
+{
+    type Output = Self;
+
+    fn add(self, other: &Self) -> Self::Output {
+        let (sign, digits) =
+            sum_signed_digits::<Digit, SHIFT>(&self.digits, self.sign, &other.digits, other.sign);
+        Self::Output { sign, digits }
+    }
+}
+
+impl<Digit: AdditiveDigit, const SEPARATOR: char, const SHIFT: usize>
+    Add<BigInt<Digit, SEPARATOR, SHIFT>> for &BigInt<Digit, SEPARATOR, SHIFT>
+{
+    type Output = BigInt<Digit, SEPARATOR, SHIFT>;
+
+    fn add(self, other: BigInt<Digit, SEPARATOR, SHIFT>) -> Self::Output {
+        let (sign, digits) =
+            sum_signed_digits::<Digit, SHIFT>(&self.digits, self.sign, &other.digits, other.sign);
+        Self::Output { sign, digits }
+    }
+}
+
+impl<Digit: AdditiveDigit, const SEPARATOR: char, const SHIFT: usize> Add
+    for &BigInt<Digit, SEPARATOR, SHIFT>
+{
+    type Output = BigInt<Digit, SEPARATOR, SHIFT>;
+
+    fn add(self, other: Self) -> Self::Output {
+        let (sign, digits) =
+            sum_signed_digits::<Digit, SHIFT>(&self.digits, self.sign, &other.digits, other.sign);
+        Self::Output { sign, digits }
+    }
+}
+
 impl<Digit: AdditiveDigit, const SEPARATOR: char, const SHIFT: usize> AddAssign
     for BigInt<Digit, SEPARATOR, SHIFT>
 {

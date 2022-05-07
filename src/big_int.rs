ab063b35f98ac3cd98e3cd4185441aa125abfb78
Azat Ibrakov <azatibrakov@gmail.com>
2022-05-07 14:57:05 +0700

    Complete 'BitOrAssign' trait implementation of 'BigInt' generic

--- src/big_int.rs
+++ src/big_int.rs
@@ -716,6 +716,28 @@ impl<Digit: BinaryDigit, const SEPARATOR: char, const SHIFT: usize> BitOrAssign
     }
 }
 
+impl<Digit: BinaryDigit, const SEPARATOR: char, const SHIFT: usize> BitOrAssign<&Self>
+    for BigInt<Digit, SEPARATOR, SHIFT>
+{
+    fn bitor_assign(&mut self, other: &Self) {
+        (self.sign, self.digits) = if self.digits.len() > other.digits.len() {
+            bitwise_or::<Digit, SHIFT>(
+                self.digits.clone(),
+                self.sign,
+                other.digits.clone(),
+                other.sign,
+            )
+        } else {
+            bitwise_or::<Digit, SHIFT>(
+                other.digits.clone(),
+                other.sign,
+                self.digits.clone(),
+                self.sign,
+            )
+        };
+    }
+}
+
 impl<Digit: BinaryDigit, const SEPARATOR: char, const SHIFT: usize> BitXor
     for BigInt<Digit, SEPARATOR, SHIFT>
 {

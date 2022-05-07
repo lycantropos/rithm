bdd0a717d53af5a107c0c987d7c6ddb17da3f3c6
Azat Ibrakov <azatibrakov@gmail.com>
2022-05-07 19:33:50 +0700

    Complete 'Sub' trait implementation of 'BigInt' generic

--- src/big_int.rs
+++ src/big_int.rs
@@ -2439,6 +2439,54 @@ impl<Digit: AdditiveDigit, const SEPARATOR: char, const SHIFT: usize> Sub
     }
 }
 
+impl<Digit: AdditiveDigit, const SEPARATOR: char, const SHIFT: usize> Sub<&Self>
+    for BigInt<Digit, SEPARATOR, SHIFT>
+{
+    type Output = Self;
+
+    fn sub(self, subtrahend: &Self) -> Self::Output {
+        let (sign, digits) = subtract_signed_digits::<Digit, SHIFT>(
+            self.sign,
+            &self.digits,
+            subtrahend.sign,
+            &subtrahend.digits,
+        );
+        Self::Output { sign, digits }
+    }
+}
+
+impl<Digit: AdditiveDigit, const SEPARATOR: char, const SHIFT: usize>
+    Sub<BigInt<Digit, SEPARATOR, SHIFT>> for &BigInt<Digit, SEPARATOR, SHIFT>
+{
+    type Output = BigInt<Digit, SEPARATOR, SHIFT>;
+
+    fn sub(self, subtrahend: BigInt<Digit, SEPARATOR, SHIFT>) -> Self::Output {
+        let (sign, digits) = subtract_signed_digits::<Digit, SHIFT>(
+            self.sign,
+            &self.digits,
+            subtrahend.sign,
+            &subtrahend.digits,
+        );
+        Self::Output { sign, digits }
+    }
+}
+
+impl<Digit: AdditiveDigit, const SEPARATOR: char, const SHIFT: usize> Sub
+    for &BigInt<Digit, SEPARATOR, SHIFT>
+{
+    type Output = BigInt<Digit, SEPARATOR, SHIFT>;
+
+    fn sub(self, subtrahend: Self) -> Self::Output {
+        let (sign, digits) = subtract_signed_digits::<Digit, SHIFT>(
+            self.sign,
+            &self.digits,
+            subtrahend.sign,
+            &subtrahend.digits,
+        );
+        Self::Output { sign, digits }
+    }
+}
+
 impl<Digit: AdditiveDigit, const SEPARATOR: char, const SHIFT: usize> SubAssign
     for BigInt<Digit, SEPARATOR, SHIFT>
 {

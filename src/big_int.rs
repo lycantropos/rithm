a7c1bd98a5dea9a840043ff763796f22d27b2844
Azat Ibrakov <azatibrakov@gmail.com>
2022-05-07 22:43:44 +0700

    Complete 'CheckedRem' trait implementation of 'BigInt' generic

--- src/big_int.rs
+++ src/big_int.rs
@@ -1321,6 +1321,39 @@ impl<Digit: DivisibleDigit, const SEPARATOR: char, const SHIFT: usize> CheckedRe
     }
 }
 
+impl<Digit: DivisibleDigit, const SEPARATOR: char, const SHIFT: usize> CheckedRem<&Self>
+    for BigInt<Digit, SEPARATOR, SHIFT>
+{
+    type Output = Option<Self>;
+
+    fn checked_rem(self, divisor: &Self) -> Self::Output {
+        checked_rem::<Digit, SHIFT>(self.sign, &self.digits, divisor.sign, &divisor.digits)
+            .map(|(sign, digits)| Self { sign, digits })
+    }
+}
+
+impl<Digit: DivisibleDigit, const SEPARATOR: char, const SHIFT: usize>
+    CheckedRem<BigInt<Digit, SEPARATOR, SHIFT>> for &BigInt<Digit, SEPARATOR, SHIFT>
+{
+    type Output = Option<BigInt<Digit, SEPARATOR, SHIFT>>;
+
+    fn checked_rem(self, divisor: BigInt<Digit, SEPARATOR, SHIFT>) -> Self::Output {
+        checked_rem::<Digit, SHIFT>(self.sign, &self.digits, divisor.sign, &divisor.digits)
+            .map(|(sign, digits)| BigInt::<Digit, SEPARATOR, SHIFT> { sign, digits })
+    }
+}
+
+impl<Digit: DivisibleDigit, const SEPARATOR: char, const SHIFT: usize> CheckedRem
+    for &BigInt<Digit, SEPARATOR, SHIFT>
+{
+    type Output = Option<BigInt<Digit, SEPARATOR, SHIFT>>;
+
+    fn checked_rem(self, divisor: Self) -> Self::Output {
+        checked_rem::<Digit, SHIFT>(self.sign, &self.digits, divisor.sign, &divisor.digits)
+            .map(|(sign, digits)| BigInt::<Digit, SEPARATOR, SHIFT> { sign, digits })
+    }
+}
+
 impl<Digit: EuclidDivisibleDigit, const SEPARATOR: char, const SHIFT: usize> CheckedRemEuclid
     for BigInt<Digit, SEPARATOR, SHIFT>
 {

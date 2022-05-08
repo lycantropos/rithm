5054e73c4592aecf1748c8930292c362822d81bb
Azat Ibrakov <azatibrakov@gmail.com>
2022-05-08 14:06:52 +0700

    Complete 'Pow' trait implementation of 'BigInt' generic

--- src/big_int/pow.rs
+++ src/big_int/pow.rs
@@ -3,14 +3,15 @@ use crate::traits::{CheckedPow, Pow};
 use super::digits::ExponentiativeDigit;
 use super::types::BigInt;
 
+const NEGATIVE_EXPONENT_MESSAGE: &str = "Exponent should be non-negative.";
+
 impl<Digit: ExponentiativeDigit, const SEPARATOR: char, const SHIFT: usize> Pow<Self>
     for BigInt<Digit, SEPARATOR, SHIFT>
 {
     type Output = Self;
 
     fn pow(self, exponent: Self) -> Self::Output {
-        self.checked_pow(exponent)
-            .unwrap_or_else(|| panic!("Exponent should be non-negative."))
+        self.checked_pow(exponent).expect(NEGATIVE_EXPONENT_MESSAGE)
     }
 }
 
@@ -20,8 +21,7 @@ impl<Digit: ExponentiativeDigit, const SEPARATOR: char, const SHIFT: usize> Pow<
     type Output = Self;
 
     fn pow(self, exponent: &Self) -> Self::Output {
-        self.checked_pow(exponent)
-            .unwrap_or_else(|| panic!("Exponent should be non-negative."))
+        self.checked_pow(exponent).expect(NEGATIVE_EXPONENT_MESSAGE)
     }
 }
 
@@ -31,8 +31,7 @@ impl<Digit: ExponentiativeDigit, const SEPARATOR: char, const SHIFT: usize>
     type Output = BigInt<Digit, SEPARATOR, SHIFT>;
 
     fn pow(self, exponent: BigInt<Digit, SEPARATOR, SHIFT>) -> Self::Output {
-        self.checked_pow(exponent)
-            .unwrap_or_else(|| panic!("Exponent should be non-negative."))
+        self.checked_pow(exponent).expect(NEGATIVE_EXPONENT_MESSAGE)
     }
 }
 
@@ -42,7 +41,6 @@ impl<Digit: ExponentiativeDigit, const SEPARATOR: char, const SHIFT: usize> Pow<
     type Output = BigInt<Digit, SEPARATOR, SHIFT>;
 
     fn pow(self, exponent: Self) -> Self::Output {
-        self.checked_pow(exponent)
-            .unwrap_or_else(|| panic!("Exponent should be non-negative."))
+        self.checked_pow(exponent).expect(NEGATIVE_EXPONENT_MESSAGE)
     }
 }

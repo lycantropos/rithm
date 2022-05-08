952d8bb57ff48d372b567cef739056a02dcbafa8
Azat Ibrakov <azatibrakov@gmail.com>
2022-05-09 00:44:32 +0700

    Complete 'CheckedShr' trait

--- src/traits.rs
+++ src/traits.rs
@@ -693,7 +693,7 @@ macro_rules! plain_checked_shl_impl {
 plain_checked_shl_impl!(i8 i16 i32 i64 i128 isize u8 u16 u32 u64 u128 usize);
 
 pub trait CheckedShr<Shift = Self> {
-    type Output: Maybe<Result = Self>;
+    type Output: Maybe;
 
     fn checked_shr(self, shift: Shift) -> Self::Output;
 }

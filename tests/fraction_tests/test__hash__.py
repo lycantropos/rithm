9d1381f23b777992d141a8f4ebd9834b5e1573ca
Azat Ibrakov <azatibrakov@gmail.com>
2022-05-10 23:43:15 +0700

    Add test for 'Fraction.__hash__' magic method

---
+++ tests/fraction_tests/test__hash__.py
@@ -0,0 +1,33 @@
+from hypothesis import given
+
+from rithm import Fraction
+from tests.utils import (FractionWithBuiltin,
+                         implication)
+from . import strategies
+
+
+@given(strategies.fractions)
+def test_basic(fraction: Fraction) -> None:
+    result = hash(fraction)
+
+    assert isinstance(result, int)
+
+
+@given(strategies.fractions)
+def test_determinism(fraction: Fraction) -> None:
+    result = hash(fraction)
+
+    assert result == hash(fraction)
+
+
+@given(strategies.fractions, strategies.fractions)
+def test_connection_with_equality(left: Fraction, right: Fraction) -> None:
+    assert implication(left == right, hash(left) == hash(right))
+
+
+@given(strategies.fractions_with_builtins)
+def test_connection_with_builtin(fraction_with_builtin: FractionWithBuiltin
+                                 ) -> None:
+    fraction, builtin_fraction = fraction_with_builtin
+
+    assert hash(fraction) == hash(builtin_fraction)

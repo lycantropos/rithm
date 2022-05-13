2cd346187fbae68f77e4177f2816657a88576c25
Azat Ibrakov <azatibrakov@gmail.com>
2022-05-13 15:02:33 +0700

    Complete 'DivAssign' trait implementation of 'Fraction' generic struct

--- src/fraction/div_assign.rs
+++ src/fraction/div_assign.rs
@@ -2,27 +2,29 @@ use std::ops::DivAssign;
 
 use crate::traits::{DivisivePartialMagma, GcdMagma, MultiplicativeMonoid, Oppositive};
 
-use super::types::{normalize_components_moduli, Fraction};
+use super::types::{normalize_components_moduli, normalize_components_sign, Fraction};
 
 impl<Component: Clone + DivisivePartialMagma + GcdMagma + MultiplicativeMonoid + Oppositive>
     DivAssign for Fraction<Component>
 {
-    fn div_assign(&mut self, other: Self) {
-        let (numerator, other_numerator) =
-            normalize_components_moduli(self.numerator.clone(), other.numerator);
-        let (denominator, other_denominator) =
-            normalize_components_moduli(self.denominator.clone(), other.denominator);
-        self.numerator = numerator * other_denominator;
-        self.denominator = denominator * other_numerator;
+    fn div_assign(&mut self, divisor: Self) {
+        let (numerator, divisor_numerator) =
+            normalize_components_moduli(self.numerator.clone(), divisor.numerator);
+        let (denominator, divisor_denominator) =
+            normalize_components_moduli(self.denominator.clone(), divisor.denominator);
+        (self.numerator, self.denominator) = normalize_components_sign(
+            numerator * divisor_denominator,
+            denominator * divisor_numerator,
+        );
     }
 }
 
 impl<Component: Clone + DivisivePartialMagma + GcdMagma + MultiplicativeMonoid + Oppositive>
     DivAssign<Component> for Fraction<Component>
 {
-    fn div_assign(&mut self, other: Component) {
-        let (numerator, other) = normalize_components_moduli(self.numerator.clone(), other);
-        self.numerator = numerator;
-        self.denominator = self.denominator.clone() * other;
+    fn div_assign(&mut self, divisor: Component) {
+        let (numerator, divisor) = normalize_components_moduli(self.numerator.clone(), divisor);
+        (self.numerator, self.denominator) =
+            normalize_components_sign(numerator, self.denominator.clone() * divisor);
     }
 }

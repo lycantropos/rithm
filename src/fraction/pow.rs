c7607e425c1dce18722f86f9b43e7cf9c08ecfe8
Azat Ibrakov <azatibrakov@gmail.com>
2022-05-13 11:37:34 +0700

    Complete 'Pow' trait implementation of 'Fraction' generic struct

--- src/fraction/pow.rs
+++ src/fraction/pow.rs
@@ -1,32 +1,33 @@
-use crate::traits::{Oppositive, Pow, Unitary, Zeroable};
+use crate::constants::UNDEFINED_DIVISION_ERROR_MESSAGE;
+use crate::traits::{CheckedPow, Oppositive, Pow, Unitary, Zeroable};
 
-use super::types::{normalize_components_sign, Fraction};
+use super::types::Fraction;
 
-impl<Component: Clone + Oppositive + Pow<Component, Output = Component> + Unitary + Zeroable>
-    Pow<Component> for Fraction<Component>
+impl<
+        Component: Clone + Oppositive + CheckedPow<Component, Output = Option<Component>> + Unitary + Zeroable,
+    > Pow<Component> for Fraction<Component>
 {
     type Output = Self;
 
     fn pow(self, exponent: Component) -> Self::Output {
-        if exponent.is_negative() {
-            if self.is_zero() {
-                panic!("Division by zero is undefined.")
-            } else {
-                let exponent = -exponent;
-                let (numerator, denominator) = normalize_components_sign(
-                    self.denominator.pow(exponent.clone()),
-                    self.numerator.pow(exponent),
-                );
-                Self {
-                    numerator,
-                    denominator,
-                }
-            }
-        } else {
-            Self {
-                numerator: self.numerator.pow(exponent.clone()),
-                denominator: self.denominator.pow(exponent),
-            }
-        }
+        self.checked_pow(exponent)
+            .expect(UNDEFINED_DIVISION_ERROR_MESSAGE)
     }
 }
+
+macro_rules! primitive_fraction_pow_impl {
+    ($($t:ty)*) => ($(
+        impl Pow<u32> for Fraction<$t> {
+            type Output = Self;
+
+            fn pow(self, exponent: u32) -> Self::Output {
+                Self::Output {
+                    numerator: self.numerator.pow(exponent),
+                    denominator: self.denominator.pow(exponent),
+                }
+            }
+        }
+        )*)
+}
+
+primitive_fraction_pow_impl!(i8 i16 i32 i64 i128 isize u8 u16 u32 u64 u128 usize);

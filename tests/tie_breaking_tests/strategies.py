7a6bfab81d29f885224e0a3638ecdded02b41c71
Azat Ibrakov <azatibrakov@gmail.com>
2022-05-15 21:50:01 +0700

    Add test for 'TieBreaking.__repr__' magic method

---
+++ tests/tie_breaking_tests/strategies.py
@@ -0,0 +1,8 @@
+from hypothesis import strategies
+
+from rithm import TieBreaking
+
+tie_breakings = strategies.sampled_from([TieBreaking.AWAY_FROM_ZERO,
+                                         TieBreaking.TO_EVEN,
+                                         TieBreaking.TO_ODD,
+                                         TieBreaking.TOWARD_ZERO])
---
+++ tests/tie_breaking_tests/test__repr__.py
@@ -0,0 +1,13 @@
+import sys
+
+from hypothesis import given
+
+from rithm import TieBreaking
+from . import strategies
+
+
+@given(strategies.tie_breakings)
+def test_round_trip(tie_breaking: TieBreaking) -> None:
+    result = repr(tie_breaking)
+
+    assert eval(result, sys.modules) is tie_breaking

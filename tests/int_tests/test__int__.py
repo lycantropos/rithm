8472060649ce6e4940d0730789b43a56f426bdea
Azat Ibrakov <azatibrakov@gmail.com>
2022-06-15 13:26:16 +0700

    Add test for 'Int.__int__' magic method

---
+++ tests/int_tests/test__int__.py
@@ -0,0 +1,11 @@
+from hypothesis import given
+
+from tests.utils import IntWithBuiltin
+from . import strategies
+
+
+@given(strategies.ints_with_builtins)
+def test_connection_with_builtin(int_with_builtin: IntWithBuiltin) -> None:
+    int_, builtin_int = int_with_builtin
+
+    assert int(int_) == builtin_int

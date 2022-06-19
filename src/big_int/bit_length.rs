a62c9dd167f56c84f6698628df9d3a8f791f82a0
Azat Ibrakov <azatibrakov@gmail.com>
2022-06-19 15:13:18 +0700

    Complete 'BitLength' trait implementation of 'BigInt' generic struct

--- src/big_int/bit_length.rs
+++ src/big_int/bit_length.rs
@@ -7,18 +7,18 @@ impl<
         Digit: BitLength<Output = usize> + ConstructibleFrom<usize> + MultiplicativeDigit,
         const SEPARATOR: char,
         const SHIFT: usize,
-    > BitLength for BigInt<Digit, SEPARATOR, SHIFT>
+    > BitLength for &BigInt<Digit, SEPARATOR, SHIFT>
 {
-    type Output = Self;
+    type Output = BigInt<Digit, SEPARATOR, SHIFT>;
 
     fn bit_length(self) -> Self::Output {
         if self.digits.len() <= usize::MAX / SHIFT {
-            Self::from(
+            Self::Output::from(
                 (self.digits.len() - 1) * SHIFT + self.digits[self.digits.len() - 1].bit_length(),
             )
         } else {
-            Self::from(self.digits.len() - 1) * Self::from(SHIFT)
-                + Self::from(self.digits[self.digits.len() - 1].bit_length())
+            Self::Output::from(self.digits.len() - 1) * Self::Output::from(SHIFT)
+                + Self::Output::from(self.digits[self.digits.len() - 1].bit_length())
         }
     }
 }

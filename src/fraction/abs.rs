cb184436469cee87720ebf0c50e4a47527681c3d
Azat Ibrakov <azatibrakov@gmail.com>
2022-07-21 09:27:07 +0700

    Complete 'Abs' trait implementation of 'Fraction' generic struct

--- src/fraction/abs.rs
+++ src/fraction/abs.rs
@@ -1,8 +1,12 @@
 use traiter::numbers::Abs;
 
+use crate::big_int::BigInt;
+
 use super::types::Fraction;
 
-impl<Component: Abs<Output = Component>> Abs for Fraction<Component> {
+impl<Digit, const SEPARATOR: char, const SHIFT: usize> Abs
+    for Fraction<BigInt<Digit, SEPARATOR, SHIFT>>
+{
     type Output = Self;
 
     fn abs(self) -> Self::Output {
@@ -12,3 +16,37 @@ impl<Component: Abs<Output = Component>> Abs for Fraction<Component> {
         }
     }
 }
+
+impl<Digit, const SEPARATOR: char, const SHIFT: usize> Abs
+    for &Fraction<BigInt<Digit, SEPARATOR, SHIFT>>
+where
+    BigInt<Digit, SEPARATOR, SHIFT>: Clone,
+    for<'a> &'a BigInt<Digit, SEPARATOR, SHIFT>:
+        Abs<Output = BigInt<Digit, SEPARATOR, SHIFT>>,
+{
+    type Output = Fraction<BigInt<Digit, SEPARATOR, SHIFT>>;
+
+    fn abs(self) -> Self::Output {
+        Self::Output {
+            numerator: (&self.numerator).abs(),
+            denominator: self.denominator.clone(),
+        }
+    }
+}
+
+macro_rules! signed_integer_fraction_abs_impl {
+    ($($integer:ty)*) => ($(
+        impl Abs for Fraction<$integer> {
+            type Output = Self;
+
+            fn abs(self) -> Self::Output {
+                Self::Output {
+                    numerator: self.numerator.abs(),
+                    denominator: self.denominator,
+                }
+            }
+        }
+    )*)
+}
+
+signed_integer_fraction_abs_impl!(i8 i16 i32 i64 i128 isize);

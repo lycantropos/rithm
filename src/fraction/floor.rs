66b8ac439464bd78e16f49e54d18f4909245f1ca
Azat Ibrakov <azatibrakov@gmail.com>
2022-07-21 10:06:59 +0700

    Complete 'Floor' trait implementation of 'Fraction' generic struct

--- src/fraction/floor.rs
+++ src/fraction/floor.rs
@@ -1,11 +1,16 @@
 use traiter::numbers::{CheckedDivEuclid, Floor};
 
+use crate::big_int::BigInt;
+
 use super::types::Fraction;
 
-impl<Component: CheckedDivEuclid<Output = Option<Component>>> Floor
-    for Fraction<Component>
+impl<Digit, const SEPARATOR: char, const SHIFT: usize> Floor
+    for Fraction<BigInt<Digit, SEPARATOR, SHIFT>>
+where
+    BigInt<Digit, SEPARATOR, SHIFT>:
+        CheckedDivEuclid<Output = Option<BigInt<Digit, SEPARATOR, SHIFT>>>,
 {
-    type Output = Component;
+    type Output = BigInt<Digit, SEPARATOR, SHIFT>;
 
     fn floor(self) -> Self::Output {
         unsafe {
@@ -15,3 +20,40 @@ impl<Component: CheckedDivEuclid<Output = Option<Component>>> Floor
         }
     }
 }
+
+impl<Digit, const SEPARATOR: char, const SHIFT: usize> Floor
+    for &Fraction<BigInt<Digit, SEPARATOR, SHIFT>>
+where
+    for<'a> &'a BigInt<Digit, SEPARATOR, SHIFT>:
+        CheckedDivEuclid<Output = Option<BigInt<Digit, SEPARATOR, SHIFT>>>,
+{
+    type Output = BigInt<Digit, SEPARATOR, SHIFT>;
+
+    fn floor(self) -> Self::Output {
+        unsafe {
+            (&self.numerator)
+                .checked_div_euclid(&self.denominator)
+                .unwrap_unchecked()
+        }
+    }
+}
+
+macro_rules! integer_fraction_floor_impl {
+    ($($integer:ty)*) => ($(
+        impl Floor for Fraction<$integer> {
+            type Output = $integer;
+
+            fn floor(self) -> Self::Output {
+                unsafe {
+                    self.numerator
+                        .checked_div_euclid(self.denominator)
+                        .unwrap_unchecked()
+                }
+            }
+        }
+    )*)
+}
+
+integer_fraction_floor_impl!(
+    i8 i16 i32 i64 i128 isize u8 u16 u32 u64 u128 usize
+);

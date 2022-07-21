1868ead33011a835ddb65915867788611dd6edfe
Azat Ibrakov <azatibrakov@gmail.com>
2022-07-21 09:59:56 +0700

    Complete 'Trunc' trait implementation of 'Fraction' generic struct

--- src/fraction/trunc.rs
+++ src/fraction/trunc.rs
@@ -1,21 +1,17 @@
-use std::ops::{Mul, Neg};
+use traiter::numbers::{Ceil, Floor, Signed, Trunc};
 
-use traiter::numbers::{
-    Ceil, CheckedDivEuclid, Floor, Signed, Trunc, Unitary,
-};
+use crate::big_int::BigInt;
 
 use super::types::Fraction;
 
-impl<
-        Component: Clone
-            + CheckedDivEuclid<Output = Option<Component>>
-            + Mul<Output = Component>
-            + Neg<Output = Component>
-            + Signed
-            + Unitary,
-    > Trunc for Fraction<Component>
+impl<Digit, const SEPARATOR: char, const SHIFT: usize> Trunc
+    for Fraction<BigInt<Digit, SEPARATOR, SHIFT>>
+where
+    Self: Ceil<Output = BigInt<Digit, SEPARATOR, SHIFT>>
+        + Floor<Output = BigInt<Digit, SEPARATOR, SHIFT>>
+        + Signed,
 {
-    type Output = Component;
+    type Output = BigInt<Digit, SEPARATOR, SHIFT>;
 
     fn trunc(self) -> Self::Output {
         if self.is_negative() {
@@ -25,3 +21,36 @@ impl<
         }
     }
 }
+
+macro_rules! signed_integer_fraction_trunc_impl {
+    ($($integer:ty)*) => ($(
+        impl Trunc for Fraction<$integer> {
+            type Output = $integer;
+
+            fn trunc(self) -> Self::Output {
+                if self.is_negative() {
+                    self.ceil()
+                } else {
+                    self.floor()
+                }
+            }
+        }
+    )*)
+}
+
+signed_integer_fraction_trunc_impl!(i8 i16 i32 i64 i128 isize);
+
+macro_rules! unsigned_integer_fraction_trunc_impl {
+    ($($integer:ty)*) => ($(
+        impl Trunc for Fraction<$integer> {
+            type Output = $integer;
+
+            #[inline]
+            fn trunc(self) -> Self::Output {
+                self.floor()
+            }
+        }
+    )*)
+}
+
+unsigned_integer_fraction_trunc_impl!(u8 u16 u32 u64 u128 usize);

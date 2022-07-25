572bf0bfefd609429f500001c426b7c9cef90476
Azat Ibrakov <azatibrakov@gmail.com>
2022-07-25 13:06:34 +0700

    Complete 'DivEuclid' trait implementation of 'Fraction' generic struct

--- src/fraction/div_euclid.rs
+++ src/fraction/div_euclid.rs
@@ -5,11 +5,12 @@ use crate::constants::UNDEFINED_DIVISION_ERROR_MESSAGE;
 
 use super::types::Fraction;
 
-impl<Component> DivEuclid for Fraction<Component>
+impl<Digit, const SEPARATOR: char, const SHIFT: usize> DivEuclid
+    for Fraction<BigInt<Digit, SEPARATOR, SHIFT>>
 where
-    Self: CheckedDivEuclid<Output = Option<Component>>,
+    Self: CheckedDivEuclid<Output = Option<BigInt<Digit, SEPARATOR, SHIFT>>>,
 {
-    type Output = Component;
+    type Output = BigInt<Digit, SEPARATOR, SHIFT>;
 
     fn div_euclid(self, divisor: Self) -> Self::Output {
         self.checked_div_euclid(divisor)
@@ -17,13 +18,130 @@ where
     }
 }
 
-impl<Component> DivEuclid<Component> for Fraction<Component>
+impl<Digit, const SEPARATOR: char, const SHIFT: usize> DivEuclid<&Self>
+    for Fraction<BigInt<Digit, SEPARATOR, SHIFT>>
 where
-    Self: CheckedDivEuclid<Component, Output = Option<Component>>,
+    for<'a> Self: CheckedDivEuclid<
+        &'a Self,
+        Output = Option<BigInt<Digit, SEPARATOR, SHIFT>>,
+    >,
 {
-    type Output = Component;
+    type Output = BigInt<Digit, SEPARATOR, SHIFT>;
 
-    fn div_euclid(self, divisor: Component) -> Self::Output {
+    fn div_euclid(self, divisor: &Self) -> Self::Output {
+        self.checked_div_euclid(divisor)
+            .expect(UNDEFINED_DIVISION_ERROR_MESSAGE)
+    }
+}
+
+impl<Digit, const SEPARATOR: char, const SHIFT: usize>
+    DivEuclid<Fraction<BigInt<Digit, SEPARATOR, SHIFT>>>
+    for &Fraction<BigInt<Digit, SEPARATOR, SHIFT>>
+where
+    Self: CheckedDivEuclid<
+        Fraction<BigInt<Digit, SEPARATOR, SHIFT>>,
+        Output = Option<BigInt<Digit, SEPARATOR, SHIFT>>,
+    >,
+{
+    type Output = BigInt<Digit, SEPARATOR, SHIFT>;
+
+    fn div_euclid(
+        self,
+        divisor: Fraction<BigInt<Digit, SEPARATOR, SHIFT>>,
+    ) -> Self::Output {
+        self.checked_div_euclid(divisor)
+            .expect(UNDEFINED_DIVISION_ERROR_MESSAGE)
+    }
+}
+
+impl<Digit, const SEPARATOR: char, const SHIFT: usize> DivEuclid
+    for &Fraction<BigInt<Digit, SEPARATOR, SHIFT>>
+where
+    Self: CheckedDivEuclid<Output = Option<BigInt<Digit, SEPARATOR, SHIFT>>>,
+{
+    type Output = BigInt<Digit, SEPARATOR, SHIFT>;
+
+    fn div_euclid(self, divisor: Self) -> Self::Output {
+        self.checked_div_euclid(divisor)
+            .expect(UNDEFINED_DIVISION_ERROR_MESSAGE)
+    }
+}
+
+impl<Digit, const SEPARATOR: char, const SHIFT: usize>
+    DivEuclid<BigInt<Digit, SEPARATOR, SHIFT>>
+    for Fraction<BigInt<Digit, SEPARATOR, SHIFT>>
+where
+    Self: CheckedDivEuclid<
+        BigInt<Digit, SEPARATOR, SHIFT>,
+        Output = Option<BigInt<Digit, SEPARATOR, SHIFT>>,
+    >,
+{
+    type Output = BigInt<Digit, SEPARATOR, SHIFT>;
+
+    fn div_euclid(
+        self,
+        divisor: BigInt<Digit, SEPARATOR, SHIFT>,
+    ) -> Self::Output {
+        self.checked_div_euclid(divisor)
+            .expect(UNDEFINED_DIVISION_ERROR_MESSAGE)
+    }
+}
+
+impl<Digit, const SEPARATOR: char, const SHIFT: usize>
+    DivEuclid<&BigInt<Digit, SEPARATOR, SHIFT>>
+    for Fraction<BigInt<Digit, SEPARATOR, SHIFT>>
+where
+    for<'a> Self: CheckedDivEuclid<
+        &'a BigInt<Digit, SEPARATOR, SHIFT>,
+        Output = Option<BigInt<Digit, SEPARATOR, SHIFT>>,
+    >,
+{
+    type Output = BigInt<Digit, SEPARATOR, SHIFT>;
+
+    fn div_euclid(
+        self,
+        divisor: &BigInt<Digit, SEPARATOR, SHIFT>,
+    ) -> Self::Output {
+        self.checked_div_euclid(divisor)
+            .expect(UNDEFINED_DIVISION_ERROR_MESSAGE)
+    }
+}
+
+impl<Digit, const SEPARATOR: char, const SHIFT: usize>
+    DivEuclid<BigInt<Digit, SEPARATOR, SHIFT>>
+    for &Fraction<BigInt<Digit, SEPARATOR, SHIFT>>
+where
+    Self: CheckedDivEuclid<
+        BigInt<Digit, SEPARATOR, SHIFT>,
+        Output = Option<BigInt<Digit, SEPARATOR, SHIFT>>,
+    >,
+{
+    type Output = BigInt<Digit, SEPARATOR, SHIFT>;
+
+    fn div_euclid(
+        self,
+        divisor: BigInt<Digit, SEPARATOR, SHIFT>,
+    ) -> Self::Output {
+        self.checked_div_euclid(divisor)
+            .expect(UNDEFINED_DIVISION_ERROR_MESSAGE)
+    }
+}
+
+impl<Digit, const SEPARATOR: char, const SHIFT: usize>
+    DivEuclid<&BigInt<Digit, SEPARATOR, SHIFT>>
+    for &Fraction<BigInt<Digit, SEPARATOR, SHIFT>>
+where
+    for<'a> Self: CheckedDivEuclid<
+        &'a BigInt<Digit, SEPARATOR, SHIFT>,
+        Output = Option<BigInt<Digit, SEPARATOR, SHIFT>>,
+    >,
+{
+    type Output = BigInt<Digit, SEPARATOR, SHIFT>;
+
+    fn div_euclid(
+        self,
+        divisor: &BigInt<Digit, SEPARATOR, SHIFT>,
+    ) -> Self::Output {
         self.checked_div_euclid(divisor)
             .expect(UNDEFINED_DIVISION_ERROR_MESSAGE)
     }
@@ -42,8 +160,81 @@ where
     }
 }
 
-macro_rules! signed_integer_div_euclid_fraction_impl {
+impl<Digit, const SEPARATOR: char, const SHIFT: usize>
+    DivEuclid<&Fraction<Self>> for BigInt<Digit, SEPARATOR, SHIFT>
+where
+    for<'a> Self: CheckedDivEuclid<&'a Fraction<Self>, Output = Option<Self>>,
+{
+    type Output = Self;
+
+    fn div_euclid(self, divisor: &Fraction<Self>) -> Self::Output {
+        self.checked_div_euclid(divisor)
+            .expect(UNDEFINED_DIVISION_ERROR_MESSAGE)
+    }
+}
+
+impl<Digit, const SEPARATOR: char, const SHIFT: usize>
+    DivEuclid<Fraction<BigInt<Digit, SEPARATOR, SHIFT>>>
+    for &BigInt<Digit, SEPARATOR, SHIFT>
+where
+    Self: CheckedDivEuclid<
+        Fraction<BigInt<Digit, SEPARATOR, SHIFT>>,
+        Output = Option<BigInt<Digit, SEPARATOR, SHIFT>>,
+    >,
+{
+    type Output = BigInt<Digit, SEPARATOR, SHIFT>;
+
+    fn div_euclid(
+        self,
+        divisor: Fraction<BigInt<Digit, SEPARATOR, SHIFT>>,
+    ) -> Self::Output {
+        self.checked_div_euclid(divisor)
+            .expect(UNDEFINED_DIVISION_ERROR_MESSAGE)
+    }
+}
+
+impl<Digit, const SEPARATOR: char, const SHIFT: usize>
+    DivEuclid<&Fraction<BigInt<Digit, SEPARATOR, SHIFT>>>
+    for &BigInt<Digit, SEPARATOR, SHIFT>
+where
+    for<'a> &'a BigInt<Digit, SEPARATOR, SHIFT>: CheckedDivEuclid<
+        &'a Fraction<BigInt<Digit, SEPARATOR, SHIFT>>,
+        Output = Option<BigInt<Digit, SEPARATOR, SHIFT>>,
+    >,
+{
+    type Output = BigInt<Digit, SEPARATOR, SHIFT>;
+
+    fn div_euclid(
+        self,
+        divisor: &Fraction<BigInt<Digit, SEPARATOR, SHIFT>>,
+    ) -> Self::Output {
+        self.checked_div_euclid(divisor)
+            .expect(UNDEFINED_DIVISION_ERROR_MESSAGE)
+    }
+}
+
+macro_rules! integer_fraction_div_euclid_impl {
     ($($integer:ty)*) => ($(
+        impl DivEuclid for Fraction<$integer>
+        {
+            type Output = $integer;
+
+            fn div_euclid(self, divisor: Self) -> Self::Output {
+                self.checked_div_euclid(divisor)
+                    .expect(UNDEFINED_DIVISION_ERROR_MESSAGE)
+            }
+        }
+
+        impl DivEuclid<$integer> for Fraction<$integer>
+        {
+            type Output = $integer;
+
+            fn div_euclid(self, divisor: $integer) -> Self::Output {
+                self.checked_div_euclid(divisor)
+                    .expect(UNDEFINED_DIVISION_ERROR_MESSAGE)
+            }
+        }
+
         impl DivEuclid<Fraction<Self>> for $integer {
             type Output = Self;
 
@@ -55,4 +246,6 @@ macro_rules! signed_integer_div_euclid_fraction_impl {
     )*)
 }
 
-signed_integer_div_euclid_fraction_impl!(i8 i16 i32 i64 i128 isize);
+integer_fraction_div_euclid_impl!(
+    i8 i16 i32 i64 i128 isize u8 u16 u32 u64 u128 usize
+);

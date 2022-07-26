eece7644b63ca47bddaac6a43b168e22d5003f2d
Azat Ibrakov <azatibrakov@gmail.com>
2022-07-26 20:22:59 +0700

    Complete 'Hash' trait implementation of 'Fraction' generic struct

--- src/fraction/hash.rs
+++ src/fraction/hash.rs
@@ -1,10 +1,31 @@
 use std::hash::{Hash, Hasher};
 
+use crate::big_int::BigInt;
+
 use super::types::Fraction;
 
-impl<Component: Hash> Hash for Fraction<Component> {
+impl<Digit, const SEPARATOR: char, const SHIFT: usize> Hash
+    for Fraction<BigInt<Digit, SEPARATOR, SHIFT>>
+where
+    BigInt<Digit, SEPARATOR, SHIFT>: Hash,
+{
     fn hash<H: Hasher>(&self, state: &mut H) {
         self.numerator.hash(state);
         self.denominator.hash(state);
     }
 }
+
+macro_rules! integer_fraction_hash_impl {
+    ($($integer:ty)*) => ($(
+        impl Hash for Fraction<$integer> {
+            fn hash<H: Hasher>(&self, state: &mut H) {
+                self.numerator.hash(state);
+                self.denominator.hash(state);
+            }
+        }
+    )*)
+}
+
+integer_fraction_hash_impl!(
+    i8 i16 i32 i64 i128 isize u8 u16 u32 u64 u128 usize
+);

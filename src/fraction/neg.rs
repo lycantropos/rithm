07e9386e54978df1ceed0f31e41d388ef3f1d074
Azat Ibrakov <azatibrakov@gmail.com>
2022-07-26 18:08:04 +0700

    Complete 'Neg' trait implementation of 'Fraction' generic struct

--- src/fraction/neg.rs
+++ src/fraction/neg.rs
@@ -1,8 +1,15 @@
 use std::ops::Neg;
 
+use crate::big_int::BigInt;
+
 use super::types::Fraction;
 
-impl<Component: Neg<Output = Component>> Neg for Fraction<Component> {
+impl<Digit, const SEPARATOR: char, const SHIFT: usize> Neg
+    for Fraction<BigInt<Digit, SEPARATOR, SHIFT>>
+where
+    BigInt<Digit, SEPARATOR, SHIFT>:
+        Neg<Output = BigInt<Digit, SEPARATOR, SHIFT>>,
+{
     type Output = Self;
 
     fn neg(self) -> Self::Output {
@@ -12,3 +19,37 @@ impl<Component: Neg<Output = Component>> Neg for Fraction<Component> {
         }
     }
 }
+
+impl<Digit, const SEPARATOR: char, const SHIFT: usize> Neg
+    for &Fraction<BigInt<Digit, SEPARATOR, SHIFT>>
+where
+    BigInt<Digit, SEPARATOR, SHIFT>: Clone,
+    for<'a> &'a BigInt<Digit, SEPARATOR, SHIFT>:
+        Neg<Output = BigInt<Digit, SEPARATOR, SHIFT>>,
+{
+    type Output = Fraction<BigInt<Digit, SEPARATOR, SHIFT>>;
+
+    fn neg(self) -> Self::Output {
+        Self::Output {
+            numerator: -(&self.numerator),
+            denominator: self.denominator.clone(),
+        }
+    }
+}
+
+macro_rules! signed_integer_fraction_neg_impl {
+    ($($integer:ty)*) => ($(
+        impl Neg for Fraction<$integer> {
+            type Output = Self;
+
+            fn neg(self) -> Self::Output {
+                Self::Output {
+                    numerator: -self.numerator,
+                    denominator: self.denominator,
+                }
+            }
+        }
+    )*)
+}
+
+signed_integer_fraction_neg_impl!(i8 i16 i32 i64 i128 isize);

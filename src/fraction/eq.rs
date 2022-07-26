53081ecdfbf05c712da05495d7d4ba4c2b04f683
Azat Ibrakov <azatibrakov@gmail.com>
2022-07-26 19:19:39 +0700

    Complete 'Eq' trait implementation of 'Fraction' generic struct

--- src/fraction/eq.rs
+++ src/fraction/eq.rs
@@ -1,3 +1,20 @@
+use crate::big_int::BigInt;
+
 use super::types::Fraction;
 
-impl<Component: Eq> Eq for Fraction<Component> where Self: PartialEq {}
+impl<Digit, const SEPARATOR: char, const SHIFT: usize> Eq
+    for Fraction<BigInt<Digit, SEPARATOR, SHIFT>>
+where
+    Self: PartialEq,
+{
+}
+
+macro_rules! integer_fraction_eq_impl {
+    ($($integer:ty)*) => ($(
+        impl Eq for Fraction<$integer> where Self: PartialEq {}
+    )*)
+}
+
+integer_fraction_eq_impl!(
+    i8 i16 i32 i64 i128 isize u8 u16 u32 u64 u128 usize
+);

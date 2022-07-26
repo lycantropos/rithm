1901d5a7ea9a875fd28b56e9c5b3392505539c7f
Azat Ibrakov <azatibrakov@gmail.com>
2022-07-26 18:13:28 +0700

    Complete 'Ord' trait implementation of 'Fraction' generic struct

--- src/fraction/ord.rs
+++ src/fraction/ord.rs
@@ -1,12 +1,35 @@
 use std::cmp::Ordering;
 use std::ops::Mul;
 
+use crate::big_int::BigInt;
+
 use super::types::Fraction;
 
-impl<Component: Clone + Eq + Mul<Output = Component> + Ord> Ord
-    for Fraction<Component>
+impl<Digit, const SEPARATOR: char, const SHIFT: usize> Ord
+    for Fraction<BigInt<Digit, SEPARATOR, SHIFT>>
+where
+    for<'a> &'a BigInt<Digit, SEPARATOR, SHIFT>:
+        Mul<Output = BigInt<Digit, SEPARATOR, SHIFT>>,
+    BigInt<Digit, SEPARATOR, SHIFT>: Ord,
+    Self: Eq + PartialOrd,
 {
     fn cmp(&self, other: &Self) -> Ordering {
-        unsafe { self.partial_cmp(other).unwrap_unchecked() }
+        (&self.numerator * &other.denominator)
+            .cmp(&(&self.denominator * &other.numerator))
     }
 }
+
+macro_rules! integer_fraction_ord_impl {
+    ($($integer:ty)*) => ($(
+        impl Ord for Fraction<$integer> {
+            fn cmp(&self, other: &Self) -> Ordering {
+                (self.numerator * other.denominator)
+                    .cmp(&(self.denominator * other.numerator))
+            }
+        }
+    )*)
+}
+
+integer_fraction_ord_impl!(
+    i8 i16 i32 i64 i128 isize u8 u16 u32 u64 u128 usize
+);

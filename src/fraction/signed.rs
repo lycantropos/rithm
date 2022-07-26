fef3e6604e303e54e05e14b0f6b43d6ce89d3802
Azat Ibrakov <azatibrakov@gmail.com>
2022-07-26 21:24:19 +0700

    Complete 'Signed' trait implementation of 'Fraction' generic struct

--- src/fraction/signed.rs
+++ src/fraction/signed.rs
@@ -1,8 +1,15 @@
-use traiter::numbers::{Sign, Signed, Unitary};
+use traiter::numbers::{Sign, Signed, Zeroable};
+
+use crate::big_int::BigInt;
 
 use super::types::Fraction;
 
-impl<Component: Signed + Unitary> Signed for Fraction<Component> {
+impl<Digit, const SEPARATOR: char, const SHIFT: usize> Signed
+    for Fraction<BigInt<Digit, SEPARATOR, SHIFT>>
+where
+    BigInt<Digit, SEPARATOR, SHIFT>: Signed,
+    Self: Zeroable,
+{
     fn is_negative(&self) -> bool {
         self.numerator.is_negative()
     }
@@ -15,3 +22,23 @@ impl<Component: Signed + Unitary> Signed for Fraction<Component> {
         self.numerator.sign()
     }
 }
+
+macro_rules! signed_integer_fraction_signed_impl {
+    ($($integer:ty)*) => ($(
+        impl Signed for Fraction<$integer> {
+            fn is_negative(&self) -> bool {
+                self.numerator.is_negative()
+            }
+
+            fn is_positive(&self) -> bool {
+                self.numerator.is_positive()
+            }
+
+            fn sign(&self) -> Sign {
+                self.numerator.sign()
+            }
+        }
+    )*)
+}
+
+signed_integer_fraction_signed_impl!(i8 i16 i32 i64 i128 isize);

b1b926d294b96c8d273991e0b56b4e6337f43b45
Azat Ibrakov <azatibrakov@gmail.com>
2022-07-26 21:55:08 +0700

    Complete 'AddAssign' trait implementation of 'Fraction' generic struct

--- src/fraction/add_assign.rs
+++ src/fraction/add_assign.rs
@@ -1,16 +1,14 @@
 use std::ops::{Add, AddAssign, Mul};
 
-use super::types::{Fraction, NormalizeModuli};
 use crate::big_int::BigInt;
+
+use super::types::{Fraction, NormalizeModuli};
 
 impl<Digit, const SEPARATOR: char, const SHIFT: usize> AddAssign
     for Fraction<BigInt<Digit, SEPARATOR, SHIFT>>
 where
     for<'a> BigInt<Digit, SEPARATOR, SHIFT>: Add<Output = BigInt<Digit, SEPARATOR, SHIFT>>
-        + Mul<
-            &'a BigInt<Digit, SEPARATOR, SHIFT>,
-            Output = BigInt<Digit, SEPARATOR, SHIFT>,
-        > + NormalizeModuli<
+        + NormalizeModuli<
             Output = (
                 BigInt<Digit, SEPARATOR, SHIFT>,
                 BigInt<Digit, SEPARATOR, SHIFT>,
@@ -25,7 +23,7 @@ where
     fn add_assign(&mut self, other: Self) {
         (self.numerator, self.denominator) = (&self.numerator
             * &other.denominator
-            + other.numerator * &self.denominator)
+            + &self.denominator * other.numerator)
             .normalize_moduli(&self.denominator * other.denominator);
     }
 }
@@ -46,7 +44,7 @@ where
     fn add_assign(&mut self, other: &Self) {
         (self.numerator, self.denominator) = (&self.numerator
             * &other.denominator
-            + &other.numerator * &self.denominator)
+            + &self.denominator * &other.numerator)
             .normalize_moduli(&self.denominator * &other.denominator);
     }
 }
@@ -55,24 +53,24 @@ impl<Digit, const SEPARATOR: char, const SHIFT: usize>
     AddAssign<BigInt<Digit, SEPARATOR, SHIFT>>
     for Fraction<BigInt<Digit, SEPARATOR, SHIFT>>
 where
-    for<'a> BigInt<Digit, SEPARATOR, SHIFT>: Mul<
-            &'a BigInt<Digit, SEPARATOR, SHIFT>,
-            Output = BigInt<Digit, SEPARATOR, SHIFT>,
-        > + NormalizeModuli<
-            &'a BigInt<Digit, SEPARATOR, SHIFT>,
-            Output = (
-                BigInt<Digit, SEPARATOR, SHIFT>,
-                BigInt<Digit, SEPARATOR, SHIFT>,
-            ),
-        >,
-    for<'a> &'a BigInt<Digit, SEPARATOR, SHIFT>: Add<
-        BigInt<Digit, SEPARATOR, SHIFT>,
-        Output = BigInt<Digit, SEPARATOR, SHIFT>,
+    for<'a> BigInt<Digit, SEPARATOR, SHIFT>: NormalizeModuli<
+        &'a BigInt<Digit, SEPARATOR, SHIFT>,
+        Output = (
+            BigInt<Digit, SEPARATOR, SHIFT>,
+            BigInt<Digit, SEPARATOR, SHIFT>,
+        ),
     >,
+    for<'a> &'a BigInt<Digit, SEPARATOR, SHIFT>: Add<
+            BigInt<Digit, SEPARATOR, SHIFT>,
+            Output = BigInt<Digit, SEPARATOR, SHIFT>,
+        > + Mul<
+            BigInt<Digit, SEPARATOR, SHIFT>,
+            Output = BigInt<Digit, SEPARATOR, SHIFT>,
+        >,
 {
     fn add_assign(&mut self, other: BigInt<Digit, SEPARATOR, SHIFT>) {
         (self.numerator, self.denominator) = (&self.numerator
-            + other * &self.denominator)
+            + &self.denominator * other)
             .normalize_moduli(&self.denominator);
     }
 }
@@ -95,18 +93,18 @@ where
 {
     fn add_assign(&mut self, other: &BigInt<Digit, SEPARATOR, SHIFT>) {
         (self.numerator, self.denominator) = (&self.numerator
-            + other * &self.denominator)
+            + &self.denominator * other)
             .normalize_moduli(&self.denominator);
     }
 }
 
-macro_rules! integer_fraction_add_impl {
+macro_rules! integer_fraction_add_assign_impl {
     ($($integer:ty)*) => ($(
         impl AddAssign for Fraction<$integer> {
             fn add_assign(&mut self, other: Self) {
                 (self.numerator, self.denominator) = (self.numerator
                     * other.denominator
-                    + other.numerator * self.denominator)
+                    + self.denominator * other.numerator)
                     .normalize_moduli(self.denominator * other.denominator);
             }
         }
@@ -114,13 +112,13 @@ macro_rules! integer_fraction_add_impl {
         impl AddAssign<$integer> for Fraction<$integer> {
             fn add_assign(&mut self, other: $integer) {
                 (self.numerator, self.denominator) = (self.numerator
-                    + other * self.denominator)
+                    + self.denominator * other)
                     .normalize_moduli(self.denominator);
             }
         }
     )*)
 }
 
-integer_fraction_add_impl!(
+integer_fraction_add_assign_impl!(
     i8 i16 i32 i64 i128 isize u8 u16 u32 u64 u128 usize
 );

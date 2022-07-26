606189e1c38f8e7aa4e07daf304a17a110782d50
Azat Ibrakov <azatibrakov@gmail.com>
2022-07-26 22:06:42 +0700

    Complete 'SubAssign' trait implementation of 'Fraction' generic struct

--- src/fraction/sub_assign.rs
+++ src/fraction/sub_assign.rs
@@ -1,34 +1,123 @@
 use std::ops::{Mul, Sub, SubAssign};
 
+use crate::big_int::BigInt;
+
 use super::types::{Fraction, NormalizeModuli};
 
-impl<
-        Component: Clone
-            + Mul<Output = Component>
-            + NormalizeModuli<Output = (Component, Component)>
-            + Sub<Output = Component>,
-    > SubAssign for Fraction<Component>
+impl<Digit, const SEPARATOR: char, const SHIFT: usize> SubAssign
+    for Fraction<BigInt<Digit, SEPARATOR, SHIFT>>
+where
+    for<'a> BigInt<Digit, SEPARATOR, SHIFT>: NormalizeModuli<
+            Output = (
+                BigInt<Digit, SEPARATOR, SHIFT>,
+                BigInt<Digit, SEPARATOR, SHIFT>,
+            ),
+        > + Sub<Output = BigInt<Digit, SEPARATOR, SHIFT>>,
+    for<'a> &'a BigInt<Digit, SEPARATOR, SHIFT>: Mul<Output = BigInt<Digit, SEPARATOR, SHIFT>>
+        + Mul<
+            BigInt<Digit, SEPARATOR, SHIFT>,
+            Output = BigInt<Digit, SEPARATOR, SHIFT>,
+        >,
 {
-    fn sub_assign(&mut self, subtrahend: Self) {
-        (self.numerator, self.denominator) = Component::normalize_moduli(
-            self.numerator.clone() * subtrahend.denominator.clone()
-                - subtrahend.numerator * self.denominator.clone(),
-            self.denominator.clone() * subtrahend.denominator,
-        );
+    fn sub_assign(&mut self, other: Self) {
+        (self.numerator, self.denominator) = (&self.numerator
+            * &other.denominator
+            - &self.denominator * other.numerator)
+            .normalize_moduli(&self.denominator * other.denominator);
     }
 }
 
-impl<
-        Component: Clone
-            + Mul<Output = Component>
-            + NormalizeModuli<Output = (Component, Component)>
-            + Sub<Output = Component>,
-    > SubAssign<Component> for Fraction<Component>
+impl<Digit, const SEPARATOR: char, const SHIFT: usize> SubAssign<&Self>
+    for Fraction<BigInt<Digit, SEPARATOR, SHIFT>>
+where
+    for<'a> BigInt<Digit, SEPARATOR, SHIFT>: NormalizeModuli<
+            Output = (
+                BigInt<Digit, SEPARATOR, SHIFT>,
+                BigInt<Digit, SEPARATOR, SHIFT>,
+            ),
+        > + Sub<Output = BigInt<Digit, SEPARATOR, SHIFT>>,
+    for<'a> &'a BigInt<Digit, SEPARATOR, SHIFT>:
+        Mul<Output = BigInt<Digit, SEPARATOR, SHIFT>>,
 {
-    fn sub_assign(&mut self, subtrahend: Component) {
-        (self.numerator, self.denominator) = Component::normalize_moduli(
-            self.numerator.clone() - subtrahend * self.denominator.clone(),
-            self.denominator.clone(),
-        );
+    fn sub_assign(&mut self, other: &Self) {
+        (self.numerator, self.denominator) = (&self.numerator
+            * &other.denominator
+            - &self.denominator * &other.numerator)
+            .normalize_moduli(&self.denominator * &other.denominator);
     }
 }
+
+impl<Digit, const SEPARATOR: char, const SHIFT: usize>
+    SubAssign<BigInt<Digit, SEPARATOR, SHIFT>>
+    for Fraction<BigInt<Digit, SEPARATOR, SHIFT>>
+where
+    for<'a> BigInt<Digit, SEPARATOR, SHIFT>: NormalizeModuli<
+        &'a BigInt<Digit, SEPARATOR, SHIFT>,
+        Output = (
+            BigInt<Digit, SEPARATOR, SHIFT>,
+            BigInt<Digit, SEPARATOR, SHIFT>,
+        ),
+    >,
+    for<'a> &'a BigInt<Digit, SEPARATOR, SHIFT>: Mul<
+            BigInt<Digit, SEPARATOR, SHIFT>,
+            Output = BigInt<Digit, SEPARATOR, SHIFT>,
+        > + Sub<
+            BigInt<Digit, SEPARATOR, SHIFT>,
+            Output = BigInt<Digit, SEPARATOR, SHIFT>,
+        >,
+{
+    fn sub_assign(&mut self, other: BigInt<Digit, SEPARATOR, SHIFT>) {
+        (self.numerator, self.denominator) = (&self.numerator
+            - &self.denominator * other)
+            .normalize_moduli(&self.denominator);
+    }
+}
+
+impl<Digit, const SEPARATOR: char, const SHIFT: usize>
+    SubAssign<&BigInt<Digit, SEPARATOR, SHIFT>>
+    for Fraction<BigInt<Digit, SEPARATOR, SHIFT>>
+where
+    for<'a> BigInt<Digit, SEPARATOR, SHIFT>: NormalizeModuli<
+        &'a BigInt<Digit, SEPARATOR, SHIFT>,
+        Output = (
+            BigInt<Digit, SEPARATOR, SHIFT>,
+            BigInt<Digit, SEPARATOR, SHIFT>,
+        ),
+    >,
+    for<'a> &'a BigInt<Digit, SEPARATOR, SHIFT>: Mul<Output = BigInt<Digit, SEPARATOR, SHIFT>>
+        + Sub<
+            BigInt<Digit, SEPARATOR, SHIFT>,
+            Output = BigInt<Digit, SEPARATOR, SHIFT>,
+        >,
+{
+    fn sub_assign(&mut self, other: &BigInt<Digit, SEPARATOR, SHIFT>) {
+        (self.numerator, self.denominator) = (&self.numerator
+            - &self.denominator * other)
+            .normalize_moduli(&self.denominator);
+    }
+}
+
+macro_rules! integer_fraction_sub_assign_impl {
+    ($($integer:ty)*) => ($(
+        impl SubAssign for Fraction<$integer> {
+            fn sub_assign(&mut self, other: Self) {
+                (self.numerator, self.denominator) = (self.numerator
+                    * other.denominator
+                    - self.denominator * other.numerator)
+                    .normalize_moduli(self.denominator * other.denominator);
+            }
+        }
+
+        impl SubAssign<$integer> for Fraction<$integer> {
+            fn sub_assign(&mut self, other: $integer) {
+                (self.numerator, self.denominator) = (self.numerator
+                    - self.denominator * other)
+                    .normalize_moduli(self.denominator);
+            }
+        }
+    )*)
+}
+
+integer_fraction_sub_assign_impl!(
+    i8 i16 i32 i64 i128 isize u8 u16 u32 u64 u128 usize
+);

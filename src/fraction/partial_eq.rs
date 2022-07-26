e233f6b91915cdda75221813545b05bddc9013c0
Azat Ibrakov <azatibrakov@gmail.com>
2022-07-26 18:24:32 +0700

    Complete 'PartialEq' trait implementation of 'Fraction' generic struct

--- src/fraction/partial_eq.rs
+++ src/fraction/partial_eq.rs
@@ -4,43 +4,62 @@ use crate::big_int::BigInt;
 
 use super::types::Fraction;
 
-impl<Component: PartialEq> PartialEq for Fraction<Component> {
+impl<Digit, const SEPARATOR: char, const SHIFT: usize> PartialEq
+    for Fraction<BigInt<Digit, SEPARATOR, SHIFT>>
+where
+    BigInt<Digit, SEPARATOR, SHIFT>: PartialEq,
+{
     fn eq(&self, other: &Self) -> bool {
         self.numerator.eq(&other.numerator)
             && self.denominator.eq(&other.denominator)
     }
 }
 
-impl<Component: PartialEq + Unitary> PartialEq<Component>
-    for Fraction<Component>
+impl<Digit, const SEPARATOR: char, const SHIFT: usize>
+    PartialEq<BigInt<Digit, SEPARATOR, SHIFT>>
+    for Fraction<BigInt<Digit, SEPARATOR, SHIFT>>
+where
+    BigInt<Digit, SEPARATOR, SHIFT>: PartialEq + Unitary,
 {
-    fn eq(&self, other: &Component) -> bool {
+    fn eq(&self, other: &BigInt<Digit, SEPARATOR, SHIFT>) -> bool {
         self.denominator.is_one() && self.numerator.eq(other)
     }
 }
 
-impl<
-        Component: PartialEq<Self> + Unitary,
-        Digit,
-        const SEPARATOR: char,
-        const SHIFT: usize,
-    > PartialEq<Fraction<Component>> for BigInt<Digit, SEPARATOR, SHIFT>
+impl<Digit, const SEPARATOR: char, const SHIFT: usize>
+    PartialEq<Fraction<BigInt<Digit, SEPARATOR, SHIFT>>>
+    for BigInt<Digit, SEPARATOR, SHIFT>
+where
+    BigInt<Digit, SEPARATOR, SHIFT>: PartialEq + Unitary,
 {
-    fn eq(&self, other: &Fraction<Component>) -> bool {
+    fn eq(&self, other: &Fraction<BigInt<Digit, SEPARATOR, SHIFT>>) -> bool {
         other.denominator.is_one() && other.numerator.eq(self)
     }
 }
 
-macro_rules! signed_integer_partial_eq_fraction_impl {
+macro_rules! integer_fraction_partial_eq_impl {
     ($($integer:ty)*) => ($(
-        impl<Component: PartialEq<$integer> + Unitary>
-            PartialEq<Fraction<Component>> for $integer
-        {
-            fn eq(&self, other: &Fraction<Component>) -> bool {
+        impl PartialEq for Fraction<$integer> {
+            fn eq(&self, other: &Self) -> bool {
+                self.numerator.eq(&other.numerator)
+                    && self.denominator.eq(&other.denominator)
+            }
+        }
+
+        impl PartialEq<$integer> for Fraction<$integer> {
+            fn eq(&self, other: &$integer) -> bool {
+                self.denominator.is_one() && self.numerator.eq(other)
+            }
+        }
+
+        impl PartialEq<Fraction<$integer>> for $integer {
+            fn eq(&self, other: &Fraction<$integer>) -> bool {
                 other.denominator.is_one() && other.numerator.eq(self)
             }
         }
     )*)
 }
 
-signed_integer_partial_eq_fraction_impl!(i8 i16 i32 i64 i128 isize);
+integer_fraction_partial_eq_impl!(
+    i8 i16 i32 i64 i128 isize u8 u16 u32 u64 u128 usize
+);

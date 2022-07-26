3e09e33c82120d9ad4da53fc903ec49c1774b297
Azat Ibrakov <azatibrakov@gmail.com>
2022-07-26 19:02:23 +0700

    Complete 'PartialOrd' trait implementation of 'Fraction' generic struct

--- src/fraction/partial_ord.rs
+++ src/fraction/partial_ord.rs
@@ -1,75 +1,79 @@
 use std::cmp::Ordering;
 use std::ops::Mul;
 
-use traiter::numbers::Unitary;
-
 use crate::big_int::BigInt;
 
 use super::types::Fraction;
 
-impl<Component: Clone + Mul<Output = Component> + PartialOrd> PartialOrd
-    for Fraction<Component>
+impl<Digit, const SEPARATOR: char, const SHIFT: usize> PartialOrd
+    for Fraction<BigInt<Digit, SEPARATOR, SHIFT>>
+where
+    for<'a> &'a BigInt<Digit, SEPARATOR, SHIFT>:
+        Mul<Output = BigInt<Digit, SEPARATOR, SHIFT>>,
+    BigInt<Digit, SEPARATOR, SHIFT>: PartialOrd,
+    Self: PartialEq,
 {
     fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        (self.numerator.clone() * other.denominator.clone())
-            .partial_cmp(&(other.numerator.clone() * self.denominator.clone()))
+        (&self.numerator * &other.denominator)
+            .partial_cmp(&(&self.denominator * &other.numerator))
     }
 }
 
-impl<Component: Clone + Mul<Output = Component> + PartialOrd + Unitary>
-    PartialOrd<Component> for Fraction<Component>
-{
-    fn partial_cmp(&self, other: &Component) -> Option<Ordering> {
-        self.numerator
-            .partial_cmp(&(other.clone() * self.denominator.clone()))
-    }
-}
-
-impl<Component: Clone, Digit, const SEPARATOR: char, const SHIFT: usize>
-    PartialOrd<Fraction<Component>> for BigInt<Digit, SEPARATOR, SHIFT>
+impl<Digit, const SEPARATOR: char, const SHIFT: usize>
+    PartialOrd<BigInt<Digit, SEPARATOR, SHIFT>>
+    for Fraction<BigInt<Digit, SEPARATOR, SHIFT>>
 where
-    Self: Clone
-        + Mul<Component, Output = Self>
-        + PartialEq<Fraction<Component>>
-        + PartialOrd<Component>,
+    for<'a> &'a BigInt<Digit, SEPARATOR, SHIFT>:
+        Mul<Output = BigInt<Digit, SEPARATOR, SHIFT>>,
+    BigInt<Digit, SEPARATOR, SHIFT>: PartialOrd,
+    Self: PartialEq<BigInt<Digit, SEPARATOR, SHIFT>>,
 {
-    fn partial_cmp(&self, other: &Fraction<Component>) -> Option<Ordering> {
-        (self.clone() * other.denominator.clone())
-            .partial_cmp(&other.numerator)
+    fn partial_cmp(
+        &self,
+        other: &BigInt<Digit, SEPARATOR, SHIFT>,
+    ) -> Option<Ordering> {
+        self.numerator.partial_cmp(&(&self.denominator * other))
     }
 }
 
-macro_rules! signed_integer_partial_ord_fraction_impl {
-    ($($integer:ty)*) => ($(
-    impl PartialOrd<Fraction<Self>> for $integer
-    {
-        fn ge(&self, other: &Fraction<Self>) -> bool {
-            self * other.denominator >= other.numerator
-        }
-
-        fn gt(&self, other: &Fraction<Self>) -> bool {
-            self * other.denominator > other.numerator
-        }
-
-        fn le(&self, other: &Fraction<Self>) -> bool {
-            self * other.denominator <= other.numerator
-        }
-
-        fn lt(&self, other: &Fraction<Self>) -> bool {
-            self * other.denominator < other.numerator
-        }
-
-        fn partial_cmp(&self, other: &Fraction<Self>) -> Option<Ordering> {
-            Some(if self.lt(other) {
-                Ordering::Less
-            } else if self.gt(other) {
-                Ordering::Greater
-            } else {
-                Ordering::Equal
-            })
-        }
+impl<Digit, const SEPARATOR: char, const SHIFT: usize>
+    PartialOrd<Fraction<Self>> for BigInt<Digit, SEPARATOR, SHIFT>
+where
+    for<'a> &'a Self: Mul<Output = Self>,
+    Self: PartialEq<Fraction<Self>> + PartialOrd,
+{
+    fn partial_cmp(&self, other: &Fraction<Self>) -> Option<Ordering> {
+        (self * &other.denominator).partial_cmp(&other.numerator)
     }
+}
+
+macro_rules! integer_partial_ord_fraction_impl {
+    ($($integer:ty)*) => ($(
+        impl PartialOrd for Fraction<$integer> {
+            fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+                (self.numerator * other.denominator)
+                    .partial_cmp(
+                        &(self.denominator * other.numerator),
+                    )
+            }
+        }
+
+        impl PartialOrd<$integer> for Fraction<$integer> {
+            fn partial_cmp(&self, other: &$integer) -> Option<Ordering> {
+                self.numerator
+                    .partial_cmp(&(self.denominator * other))
+            }
+        }
+
+        impl PartialOrd<Fraction<Self>> for $integer
+        {
+            fn partial_cmp(&self, other: &Fraction<Self>) -> Option<Ordering> {
+                (self * other.denominator).partial_cmp(&other.numerator)
+            }
+        }
     )*)
 }
 
-signed_integer_partial_ord_fraction_impl!(i8 i16 i32 i64 i128 isize);
+integer_partial_ord_fraction_impl!(
+    i8 i16 i32 i64 i128 isize u8 u16 u32 u64 u128 usize
+);

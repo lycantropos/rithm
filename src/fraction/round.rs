0b70d803a95565630dabb7ebd65e92bfac7f726d
Azat Ibrakov <azatibrakov@gmail.com>
2022-07-27 01:35:08 +0700

    Complete 'Round' trait implementation of 'Fraction' generic struct

--- src/fraction/round.rs
+++ src/fraction/round.rs
@@ -54,6 +54,51 @@ where
     }
 }
 
+impl<Digit, const SEPARATOR: char, const SHIFT: usize> Round
+    for &Fraction<BigInt<Digit, SEPARATOR, SHIFT>>
+where
+    for<'a> &'a BigInt<Digit, SEPARATOR, SHIFT>: CheckedDivRemEuclid<
+        Output = Option<(
+            BigInt<Digit, SEPARATOR, SHIFT>,
+            BigInt<Digit, SEPARATOR, SHIFT>,
+        )>,
+    >,
+    for<'a> BigInt<Digit, SEPARATOR, SHIFT>: Add<Output = BigInt<Digit, SEPARATOR, SHIFT>>
+        + Ord
+        + Parity
+        + Shl<usize, Output = BigInt<Digit, SEPARATOR, SHIFT>>
+        + Signed
+        + Unitary,
+{
+    type Output = BigInt<Digit, SEPARATOR, SHIFT>;
+
+    fn round(self, tie_breaking: TieBreaking) -> Self::Output {
+        let (quotient, remainder) = unsafe {
+            self.numerator
+                .checked_div_rem_euclid(&self.denominator)
+                .unwrap_unchecked()
+        };
+        match (remainder << 1).cmp(&self.denominator) {
+            Ordering::Equal => {
+                if match tie_breaking {
+                    TieBreaking::AwayFromZero => !quotient.is_negative(),
+                    TieBreaking::ToEven => quotient.is_odd(),
+                    TieBreaking::ToOdd => quotient.is_even(),
+                    TieBreaking::TowardZero => quotient.is_negative(),
+                } {
+                    quotient + BigInt::<Digit, SEPARATOR, SHIFT>::one()
+                } else {
+                    quotient
+                }
+            }
+            Ordering::Greater => {
+                quotient + BigInt::<Digit, SEPARATOR, SHIFT>::one()
+            }
+            Ordering::Less => quotient,
+        }
+    }
+}
+
 macro_rules! signed_integer_fraction_round_impl {
     ($($integer:ty)*) => ($(
         impl Round for Fraction<$integer> {

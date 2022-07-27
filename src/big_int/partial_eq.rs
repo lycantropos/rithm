eae46b23d11a90431955637a85b8753b12b1e258
Azat Ibrakov <azatibrakov@gmail.com>
2022-07-27 10:06:01 +0700

    Complete 'PartialEq' trait implementation of 'BigInt' generic struct

--- src/big_int/partial_eq.rs
+++ src/big_int/partial_eq.rs
@@ -3,34 +3,40 @@ use traiter::numbers::Zeroable;
 use super::digits::{value_to_sign, DigitsFromNonZeroValue};
 use super::types::BigInt;
 
-macro_rules! primitive_partial_eq_to_big_int_impl {
-    ($($t:ty)*) => ($(
+macro_rules! integer_partial_eq_big_int_impl {
+    ($($integer:ty)*) => ($(
         impl<
-                Digit: DigitsFromNonZeroValue<$t> + PartialEq + Zeroable,
+                Digit: DigitsFromNonZeroValue<$integer> + PartialEq + Zeroable,
                 const SEPARATOR: char,
                 const SHIFT: usize,
-            > PartialEq<BigInt<Digit, SEPARATOR, SHIFT>> for $t
+            > PartialEq<BigInt<Digit, SEPARATOR, SHIFT>> for $integer
         {
             fn eq(&self, other: &BigInt<Digit, SEPARATOR, SHIFT>) -> bool {
                 value_to_sign(*self) == other.sign
                     && (self.is_zero()
-                        || Digit::digits_from_non_zero_value::<SHIFT>(*self) == other.digits)
+                        || Digit::digits_from_non_zero_value::<SHIFT>(*self)
+                            == other.digits)
             }
         }
 
         impl<
-                Digit: DigitsFromNonZeroValue<$t> + PartialEq + Zeroable,
+                Digit: DigitsFromNonZeroValue<$integer> + PartialEq + Zeroable,
                 const SEPARATOR: char,
                 const SHIFT: usize,
-            > PartialEq<$t> for BigInt<Digit, SEPARATOR, SHIFT>
+            > PartialEq<$integer> for BigInt<Digit, SEPARATOR, SHIFT>
         {
-            fn eq(&self, other: &$t) -> bool {
+            fn eq(&self, other: &$integer) -> bool {
                 self.sign == value_to_sign(*other)
                     && (self.is_zero()
-                        || self.digits == Digit::digits_from_non_zero_value::<SHIFT>(*other))
+                        || self.digits
+                            == Digit::digits_from_non_zero_value::<SHIFT>(
+                                *other,
+                            ))
             }
         }
     )*)
 }
 
-primitive_partial_eq_to_big_int_impl!(i8 i16 i32 i64 i128 isize u8 u16 u32 u64 u128 usize);
+integer_partial_eq_big_int_impl!(
+    i8 i16 i32 i64 i128 isize u8 u16 u32 u64 u128 usize
+);

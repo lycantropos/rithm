67c837e63857b513b80f3c00e7c40ac0ea9e2060
Azat Ibrakov <azatibrakov@gmail.com>
2022-07-27 20:05:26 +0700

    Complete 'Ord' trait implementation of 'BigInt' generic struct

--- src/big_int/ord.rs
+++ src/big_int/ord.rs
@@ -1,14 +1,22 @@
 use std::cmp::Ordering;
 
+use traiter::numbers::{Sign, Signed};
+
 use super::digits::compare_digits;
 use super::types::BigInt;
 
 impl<Digit: Ord, const SEPARATOR: char, const SHIFT: usize> Ord
     for BigInt<Digit, SEPARATOR, SHIFT>
+where
+    Self: Signed,
 {
     fn cmp(&self, other: &Self) -> Ordering {
         match self.sign.cmp(&other.sign) {
-            Ordering::Equal => compare_digits(&self.digits, &other.digits),
+            Ordering::Equal => match self.sign() {
+                Sign::Negative => compare_digits(&other.digits, &self.digits),
+                Sign::Positive => compare_digits(&self.digits, &other.digits),
+                Sign::Zero => Ordering::Equal,
+            },
             value => value,
         }
     }

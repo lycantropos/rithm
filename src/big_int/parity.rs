e99e95c05002af775c9e9c810291f87384439aa1
Azat Ibrakov <azatibrakov@gmail.com>
2023-06-20 21:21:52 +0200

    Complete 'Parity' trait implementation of 'BigInt' generic struct

--- src/big_int/parity.rs
+++ src/big_int/parity.rs
@@ -2,10 +2,10 @@ use traiter::numbers::Parity;
 
 use super::types::BigInt;
 
-impl<Digit, const DIGIT_BITNESS: usize> Parity
-    for &BigInt<Digit, DIGIT_BITNESS>
+impl<'a, Digit, const DIGIT_BITNESS: usize> Parity
+    for &'a BigInt<Digit, DIGIT_BITNESS>
 where
-    for<'a> &'a Digit: Parity,
+    &'a Digit: Parity,
 {
     fn is_even(self) -> bool {
         self.digits[0].is_even()
@@ -13,5 +13,18 @@ where
 
     fn is_odd(self) -> bool {
         self.digits[0].is_odd()
+    }
+}
+
+impl<Digit, const DIGIT_BITNESS: usize> Parity for BigInt<Digit, DIGIT_BITNESS>
+where
+    Digit: Parity,
+{
+    fn is_even(self) -> bool {
+        unsafe { self.digits.into_iter().next().unwrap_unchecked() }.is_even()
+    }
+
+    fn is_odd(self) -> bool {
+        unsafe { self.digits.into_iter().next().unwrap_unchecked() }.is_odd()
     }
 }

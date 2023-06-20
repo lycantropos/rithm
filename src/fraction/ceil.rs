d5e005620a33a88b068ef7ca1549357ac3435d2b
Azat Ibrakov <azatibrakov@gmail.com>
2023-06-20 21:15:55 +0200

    Complete 'Ceil' trait implementation of 'Fraction' generic struct

--- src/fraction/ceil.rs
+++ src/fraction/ceil.rs
@@ -6,6 +6,27 @@ use crate::big_int::BigInt;
 
 use super::types::Fraction;
 
+impl<'a, Digit, const DIGIT_BITNESS: usize> Ceil
+    for &'a Fraction<BigInt<Digit, DIGIT_BITNESS>>
+where
+    &'a BigInt<Digit, DIGIT_BITNESS>:
+        Neg<Output = BigInt<Digit, DIGIT_BITNESS>>,
+    BigInt<Digit, DIGIT_BITNESS>: CheckedDivEuclid<
+        &'a BigInt<Digit, DIGIT_BITNESS>,
+        Output = Option<BigInt<Digit, DIGIT_BITNESS>>,
+    >,
+{
+    type Output = BigInt<Digit, DIGIT_BITNESS>;
+
+    fn ceil(self) -> Self::Output {
+        -unsafe {
+            (-&self.numerator)
+                .checked_div_euclid(&self.denominator)
+                .unwrap_unchecked()
+        }
+    }
+}
+
 impl<Digit, const DIGIT_BITNESS: usize> Ceil
     for Fraction<BigInt<Digit, DIGIT_BITNESS>>
 where
@@ -23,29 +44,20 @@ where
     }
 }
 
-impl<Digit, const DIGIT_BITNESS: usize> Ceil
-    for &Fraction<BigInt<Digit, DIGIT_BITNESS>>
-where
-    for<'a> BigInt<Digit, DIGIT_BITNESS>: CheckedDivEuclid<
-        &'a BigInt<Digit, DIGIT_BITNESS>,
-        Output = Option<BigInt<Digit, DIGIT_BITNESS>>,
-    >,
-    for<'a> &'a BigInt<Digit, DIGIT_BITNESS>:
-        Neg<Output = BigInt<Digit, DIGIT_BITNESS>>,
-{
-    type Output = BigInt<Digit, DIGIT_BITNESS>;
-
-    fn ceil(self) -> Self::Output {
-        -unsafe {
-            (-&self.numerator)
-                .checked_div_euclid(&self.denominator)
-                .unwrap_unchecked()
-        }
-    }
-}
-
 macro_rules! signed_integer_fraction_ceil_impl {
     ($($integer:ty)*) => ($(
+        impl Ceil for &Fraction<$integer> {
+            type Output = $integer;
+
+            fn ceil(self) -> Self::Output {
+                -unsafe {
+                    (-self.numerator)
+                        .checked_div_euclid(self.denominator)
+                        .unwrap_unchecked()
+                }
+            }
+        }
+
         impl Ceil for Fraction<$integer> {
             type Output = $integer;
 
@@ -64,6 +76,23 @@ signed_integer_fraction_ceil_impl!(i8 i16 i32 i64 i128 isize);
 
 macro_rules! unsigned_integer_fraction_ceil_impl {
     ($($integer:ty)*) => ($(
+        impl Ceil for &Fraction<$integer> {
+            type Output = $integer;
+
+            fn ceil(self) -> Self::Output {
+                let (quotient, remainder) = unsafe {
+                    self.numerator
+                        .checked_div_rem_euclid(self.denominator)
+                        .unwrap_unchecked()
+                };
+                if remainder == 0 {
+                    quotient
+                } else {
+                    quotient + 1
+                }
+            }
+        }
+
         impl Ceil for Fraction<$integer> {
             type Output = $integer;
 

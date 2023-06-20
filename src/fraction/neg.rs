debdca479d6eeecc971ed8d21708f51d747ca217
Azat Ibrakov <azatibrakov@gmail.com>
2023-06-20 19:50:49 +0200

    Complete 'Neg' trait implementation of 'Fraction' generic struct

--- src/fraction/neg.rs
+++ src/fraction/neg.rs
@@ -4,6 +4,23 @@ use crate::big_int::BigInt;
 
 use super::types::Fraction;
 
+impl<'a, Digit, const DIGIT_BITNESS: usize> Neg
+    for &'a Fraction<BigInt<Digit, DIGIT_BITNESS>>
+where
+    &'a BigInt<Digit, DIGIT_BITNESS>:
+        Neg<Output = BigInt<Digit, DIGIT_BITNESS>>,
+    BigInt<Digit, DIGIT_BITNESS>: Clone,
+{
+    type Output = Fraction<BigInt<Digit, DIGIT_BITNESS>>;
+
+    fn neg(self) -> Self::Output {
+        Self::Output {
+            numerator: -(&self.numerator),
+            denominator: self.denominator.clone(),
+        }
+    }
+}
+
 impl<Digit, const DIGIT_BITNESS: usize> Neg
     for Fraction<BigInt<Digit, DIGIT_BITNESS>>
 where
@@ -19,25 +36,19 @@ where
     }
 }
 
-impl<Digit, const DIGIT_BITNESS: usize> Neg
-    for &Fraction<BigInt<Digit, DIGIT_BITNESS>>
-where
-    BigInt<Digit, DIGIT_BITNESS>: Clone,
-    for<'a> &'a BigInt<Digit, DIGIT_BITNESS>:
-        Neg<Output = BigInt<Digit, DIGIT_BITNESS>>,
-{
-    type Output = Fraction<BigInt<Digit, DIGIT_BITNESS>>;
-
-    fn neg(self) -> Self::Output {
-        Self::Output {
-            numerator: -(&self.numerator),
-            denominator: self.denominator.clone(),
-        }
-    }
-}
-
 macro_rules! signed_integer_fraction_neg_impl {
     ($($integer:ty)*) => ($(
+        impl Neg for &Fraction<$integer> {
+            type Output = Fraction<$integer>;
+
+            fn neg(self) -> Self::Output {
+                Self::Output {
+                    numerator: -self.numerator,
+                    denominator: self.denominator,
+                }
+            }
+        }
+
         impl Neg for Fraction<$integer> {
             type Output = Self;
 

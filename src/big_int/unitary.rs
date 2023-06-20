a70f3577e7af19888b06a65048920374f00003c9
Azat Ibrakov <azatibrakov@gmail.com>
2023-06-20 04:29:41 +0200

    Complete 'Unitary' trait implementation of 'BigInt' generic struct

--- src/big_int/unitary.rs
+++ src/big_int/unitary.rs
@@ -2,8 +2,20 @@ use traiter::numbers::Unitary;
 
 use super::types::BigInt;
 
+impl<'a, Digit, const DIGIT_BITNESS: usize> Unitary
+    for &'a BigInt<Digit, DIGIT_BITNESS>
+where
+    &'a Digit: Unitary,
+{
+    fn is_one(self) -> bool {
+        self.sign.is_positive()
+            && self.digits.len() == 1
+            && self.digits[0].is_one()
+    }
+}
+
 impl<Digit, const DIGIT_BITNESS: usize> Unitary
-    for &BigInt<Digit, DIGIT_BITNESS>
+    for BigInt<Digit, DIGIT_BITNESS>
 where
     for<'a> &'a Digit: Unitary,
 {

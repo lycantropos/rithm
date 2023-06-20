a1e717389bc54d6a9c6738da3b9791050a5aedc0
Azat Ibrakov <azatibrakov@gmail.com>
2023-06-20 21:13:14 +0200

    Complete 'Floor' trait implementation of 'Fraction' generic struct

--- src/fraction/floor.rs
+++ src/fraction/floor.rs
@@ -4,6 +4,23 @@ use crate::big_int::BigInt;
 
 use super::types::Fraction;
 
+impl<'a, Digit, const DIGIT_BITNESS: usize> Floor
+    for &'a Fraction<BigInt<Digit, DIGIT_BITNESS>>
+where
+    &'a BigInt<Digit, DIGIT_BITNESS>:
+        CheckedDivEuclid<Output = Option<BigInt<Digit, DIGIT_BITNESS>>>,
+{
+    type Output = BigInt<Digit, DIGIT_BITNESS>;
+
+    fn floor(self) -> Self::Output {
+        unsafe {
+            (&self.numerator)
+                .checked_div_euclid(&self.denominator)
+                .unwrap_unchecked()
+        }
+    }
+}
+
 impl<Digit, const DIGIT_BITNESS: usize> Floor
     for Fraction<BigInt<Digit, DIGIT_BITNESS>>
 where
@@ -21,25 +38,20 @@ where
     }
 }
 
-impl<Digit, const DIGIT_BITNESS: usize> Floor
-    for &Fraction<BigInt<Digit, DIGIT_BITNESS>>
-where
-    for<'a> &'a BigInt<Digit, DIGIT_BITNESS>:
-        CheckedDivEuclid<Output = Option<BigInt<Digit, DIGIT_BITNESS>>>,
-{
-    type Output = BigInt<Digit, DIGIT_BITNESS>;
-
-    fn floor(self) -> Self::Output {
-        unsafe {
-            (&self.numerator)
-                .checked_div_euclid(&self.denominator)
-                .unwrap_unchecked()
-        }
-    }
-}
-
 macro_rules! integer_fraction_floor_impl {
     ($($integer:ty)*) => ($(
+        impl Floor for &Fraction<$integer> {
+            type Output = $integer;
+
+            fn floor(self) -> Self::Output {
+                unsafe {
+                    self.numerator
+                        .checked_div_euclid(self.denominator)
+                        .unwrap_unchecked()
+                }
+            }
+        }
+
         impl Floor for Fraction<$integer> {
             type Output = $integer;
 

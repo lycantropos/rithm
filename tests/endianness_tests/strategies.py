df70cccc9db21562baea5652ff5956c515334636
Azat Ibrakov <azatibrakov@gmail.com>
2023-07-05 01:17:00 +0200

    Add test for 'Endianness.__new__' magic method

--- tests/endianness_tests/strategies.py
+++ tests/endianness_tests/strategies.py
@@ -1,5 +1,12 @@
-from hypothesis import strategies
+from hypothesis import strategies as _st
 
-from rithm.enums import Endianness
+from rithm.enums import Endianness as _Endianness
 
-endiannesses = strategies.sampled_from([Endianness.BIG, Endianness.LITTLE])
+_endiannesses = [_Endianness.BIG, _Endianness.LITTLE]
+endiannesses = _st.sampled_from(_endiannesses)
+_endiannesses_values = [endianness.value for endianness in _endiannesses]
+endiannesses_values = _st.sampled_from(_endiannesses_values)
+non_endiannesses_values = (
+        _st.text().filter(lambda value: value not in _endiannesses_values)
+        | _st.from_type(type).filter(lambda value: not isinstance(value, str))
+)
---
+++ tests/endianness_tests/test__new__.py
@@ -0,0 +1,34 @@
+from typing import Any
+
+import pytest
+from hypothesis import given
+
+from rithm.enums import Endianness
+from tests.utils import equivalence
+from . import strategies
+
+
+@given(strategies.endiannesses_values)
+def test_basic(value: str) -> None:
+    result = Endianness(value)
+
+    assert isinstance(result, Endianness)
+
+
+@given(strategies.endiannesses_values, strategies.endiannesses_values)
+def test_bijection(first: str, second: str) -> None:
+    assert equivalence(first == second,
+                       Endianness(first) is Endianness(second))
+
+
+@given(strategies.endiannesses_values)
+def test_value_round_trip(value: str) -> None:
+    result = Endianness(value)
+
+    assert result is Endianness(result.value)
+
+
+@given(strategies.non_endiannesses_values)
+def test_invalid_value(value: Any) -> None:
+    with pytest.raises(ValueError):
+        Endianness(value)

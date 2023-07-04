e6305d762deefaa7df1ccd1d958102683fd701fd
Azat Ibrakov <azatibrakov@gmail.com>
2023-07-05 01:09:00 +0200

    Add test for 'TieBreaking.__new__' magic method

--- tests/tie_breaking_tests/strategies.py
+++ tests/tie_breaking_tests/strategies.py
@@ -1,8 +1,13 @@
-from hypothesis import strategies
+from hypothesis import strategies as _st
 
-from rithm.enums import TieBreaking
+from rithm.enums import TieBreaking as _TieBreaking
 
-tie_breakings = strategies.sampled_from([TieBreaking.AWAY_FROM_ZERO,
-                                         TieBreaking.TO_EVEN,
-                                         TieBreaking.TO_ODD,
-                                         TieBreaking.TOWARD_ZERO])
+_tie_breakings = [_TieBreaking.AWAY_FROM_ZERO, _TieBreaking.TO_EVEN,
+                  _TieBreaking.TO_ODD, _TieBreaking.TOWARD_ZERO]
+tie_breakings = _st.sampled_from(_tie_breakings)
+_tie_breakings_values = [tie_breaking.value for tie_breaking in _tie_breakings]
+tie_breakings_values = _st.sampled_from(_tie_breakings_values)
+non_tie_breakings_values = (
+        _st.integers().filter(lambda value: value not in _tie_breakings_values)
+        | _st.from_type(type).filter(lambda value: not isinstance(value, int))
+)
---
+++ tests/tie_breaking_tests/test__new__.py
@@ -0,0 +1,34 @@
+from typing import Any
+
+import pytest
+from hypothesis import given
+
+from rithm.enums import TieBreaking
+from tests.utils import equivalence
+from . import strategies
+
+
+@given(strategies.tie_breakings_values)
+def test_basic(value: int) -> None:
+    result = TieBreaking(value)
+
+    assert isinstance(result, TieBreaking)
+
+
+@given(strategies.tie_breakings_values, strategies.tie_breakings_values)
+def test_bijection(first: int, second: int) -> None:
+    assert equivalence(first == second,
+                       TieBreaking(first) is TieBreaking(second))
+
+
+@given(strategies.tie_breakings_values)
+def test_value_round_trip(value: int) -> None:
+    result = TieBreaking(value)
+
+    assert result is TieBreaking(result.value)
+
+
+@given(strategies.non_tie_breakings_values)
+def test_invalid_value(value: Any) -> None:
+    with pytest.raises(ValueError):
+        TieBreaking(value)

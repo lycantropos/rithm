58d6cbc55446e58a7d309b8ae9196a31e46fcf86
Azat Ibrakov <azatibrakov@gmail.com>
2024-11-02 00:11:15 +0700

    Fix type annotations

--- setup.py
+++ setup.py
@@ -1,15 +1,20 @@
-import platform
+from __future__ import annotations
 
-from setuptools import (find_packages,
-                        setup)
+import platform
+from typing import Any
+
+from setuptools import find_packages, setup
 
 project_base_url = 'https://github.com/lycantropos/rithm/'
-parameters = dict(packages=find_packages(exclude=('tests', 'tests.*')),
-                  url=project_base_url,
-                  download_url=project_base_url + 'archive/master.zip')
+parameters: dict[str, Any] = dict(
+    packages=find_packages(exclude=('tests', 'tests.*')),
+    url=project_base_url,
+    download_url=project_base_url + 'archive/master.zip',
+)
 if platform.python_implementation() == 'CPython':
     from setuptools_rust import RustExtension
 
-    parameters.update(rust_extensions=[RustExtension('rithm._crithm')],
-                      zip_safe=False)
+    parameters.update(
+        rust_extensions=[RustExtension('rithm._crithm')], zip_safe=False
+    )
 setup(**parameters)
